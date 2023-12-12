aa6fbecd3294f258d6ec8b38409046a391b354a1
Hyungjoon Cho <selcon1996@gmail.com>
2023-12-12 16:19:19 +0900

    Fix : 예제로 사용한 코드 삭제

--- orury-api/src/main/java/org/fastcampus/oruryapi/domain/post/controller/PostController.java
+++ orury-api/src/main/java/org/fastcampus/oruryapi/domain/post/controller/PostController.java
@@ -2,8 +2,6 @@ package org.fastcampus.oruryapi.domain.post.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.fastcampus.oruryapi.global.message.error.ErrorMessage;
-import org.fastcampus.oruryapi.global.message.info.InfoMessage;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +10,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 @RestController
 public class PostController {
-    String str = InfoMessage.POST_CREATED.toString();
-    String str2 = ErrorMessage.NO_POST.toString();
 }
