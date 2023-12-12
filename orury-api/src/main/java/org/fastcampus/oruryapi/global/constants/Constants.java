40c09723d92dcf6ebefe2c5bb34e9e583fc18cb1
Hyungjoon Cho <selcon1996@gmail.com>
2023-12-12 16:14:35 +0900

    Feat : 상수 및 메시징 처리

---
+++ orury-api/src/main/java/org/fastcampus/oruryapi/global/constants/Constants.java
@@ -0,0 +1,27 @@
+package org.fastcampus.oruryapi.global.constants;
+
+import lombok.Getter;
+
+@Getter
+public enum Constants {
+    ADMIN("admin"),
+    SYSTEM("system"),
+    ROLE_USER("ROLE_USER"),
+    ROLE_ADMIN("ROLE_ADMIN"),
+
+
+    // userInfo 등 하드 코딩된 것 상수로
+    USERID("userId"),
+
+    // header 종류
+    AUTHORIZATION("Authorization"),
+    REFRESH_HEADER("Refresh-token")
+    ;
+
+    private final String message;
+
+    Constants(String message) {
+        this.message = message;
+    }
+
+}
