daf82a114b96183724577109a71e34669badd79b
Chan <ieejo716@naver.com>
2023-12-13 18:55:34 +0900

    Feature : Admin DTO
    - request, principal

---
+++ orury-admin/src/main/java/org/fastcampus/oruryadmin/domain/admin/converter/dto/AdminPrincipal.java
@@ -0,0 +1,96 @@
+package org.fastcampus.oruryadmin.domain.admin.converter.dto;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public record AdminPrincipal(
+        Long id,
+        String name,
+        String email,
+        String password,
+        Collection<? extends GrantedAuthority> authorities
+) implements UserDetails {
+    public static AdminPrincipal of(
+            Long id,
+            String name,
+            String email,
+            String password
+    ) {
+        return AdminPrincipal.of(
+                id,
+                name,
+                email,
+                password,
+                Map.of()
+        );
+    }
+
+    public static AdminPrincipal of(
+            Long id,
+            String name,
+            String email,
+            String password,
+            Map<String, Object> oAuthAttributes
+    ) {
+        Set<RoleType> roleTypes = new HashSet<>();
+        roleTypes.add(RoleType.ADMIN);
+        roleTypes.add(RoleType.USER);
+        return new AdminPrincipal(
+                id,
+                name,
+                email,
+                password,
+                roleTypes.stream().map(RoleType::getName).map(SimpleGrantedAuthority::new).collect(Collectors.toUnmodifiableSet()));
+    }
+
+    public static AdminPrincipal from(AdminDto dto) {
+        return AdminPrincipal.of(
+                dto.id(),
+                dto.name(),
+                dto.email(),
+                dto.password()
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
