932bf43246e931d96c8dc0022c7aceefdecd0d39
Chan <ieejo716@naver.com>
2023-12-14 15:33:42 +0900

    Chore : Getter 필요없어서 삭제

--- orury-admin/src/main/java/org/fastcampus/oruryadmin/global/security/dto/AuthenticationAdmin.java
+++ orury-admin/src/main/java/org/fastcampus/oruryadmin/global/security/dto/AuthenticationAdmin.java
@@ -1,13 +1,11 @@
 package org.fastcampus.oruryadmin.global.security.dto;
 
-import lombok.Getter;
 import org.fastcampus.oruryadmin.domain.admin.db.model.Admin;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import java.util.Collection;
 
-@Getter
 public class AuthenticationAdmin extends User {
 
     public AuthenticationAdmin(Admin admin, Collection<? extends GrantedAuthority> authorities) {
