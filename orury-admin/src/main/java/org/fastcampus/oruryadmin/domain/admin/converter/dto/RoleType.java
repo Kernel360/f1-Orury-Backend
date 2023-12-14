e157823a705de5ca34b0749c1fa9238176260b5b
Chan <ieejo716@naver.com>
2023-12-14 15:28:33 +0900

    Chore : RoleType 수정

--- orury-admin/src/main/java/org/fastcampus/oruryadmin/domain/admin/converter/dto/RoleType.java
+++ orury-admin/src/main/java/org/fastcampus/oruryadmin/domain/admin/converter/dto/RoleType.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum RoleType {
-    USER("ROLE_USER"),
-    ADMIN("ROLE_ADMIN");
+    ROLE_USER("ROLE_USER"),
+    ROLE_ADMIN("ROLE_ADMIN");
 
     private final String name;
 
