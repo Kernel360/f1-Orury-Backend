0a7a8a26d8cfe1e2df08d4dbcb32dce4a9370c8b
Yejin Song <yjyjsssong@gmail.com>
2023-12-18 15:26:27 +0900

    Feat : global errormessage 삭제

--- orury-api/src/main/java/org/fastcampus/oruryapi/global/error/code/UserErrorCode.java
+++ orury-api/src/main/java/org/fastcampus/oruryapi/global/error/code/UserErrorCode.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 public enum UserErrorCode implements ErrorCode{
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(),"유저를 찾을 수 없습니다.");
+    NOT_FOUND(HttpStatus.NOT_FOUND.value(),"유저를 찾을 수 없습니다.");
 
     private final int status;
     private final String message;
