cb838ab64b16b75da20078cf96312fcc63d7270b
GBGreenBravo <gbgreenbravo@gmail.com>
2023-12-19 15:57:59 +0900

    Refactor : NumberConstants Separation - 숫자형 상수 관리용 클래스 생성.   (기존의 Constants enum과 별개로 final class로 설정)

---
+++ orury-api/src/main/java/org/fastcampus/oruryapi/global/constants/NumberConstants.java
@@ -0,0 +1,15 @@
+package org.fastcampus.oruryapi.global.constants;
+
+public final class NumberConstants {
+    private NumberConstants() {}
+
+    // User Id Hard Coding (Before Adapting Security)
+    public static final Long USER_ID = 1L;
+
+    // First Pagination Cursor Input Value
+    public static final Long FIRST_CURSOR = 0L;
+    // Last Pagination Cursor Return Value
+    public static final Long LAST_CURSOR = -1L;
+    // Pagination Size
+    public static final int PAGINATION_SIZE = 10;
+}
