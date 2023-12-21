1cf4f30d0b58ac85c9ec7340c4a26e44f538714c
GBGreenBravo <gbgreenbravo@gmail.com>
2023-12-21 19:01:09 +0900

    Feat : Comment(댓글) CRUD 구현 - CommentController : 댓글 "생성/조회/수정/삭제"에 해당하는 api 1개씩 메서드 구현했습니다. - CommentService(생성, 수정) : 각 기능에 맞는 request를 받아 Dto로 변환해서 save합니다. - CommentService(삭제) : DeleteMapping이기는 하나, 실제 수행되는 작업은 수정에 가깝습니다. 정책 상, 부모댓글을 삭제하더라고 자녀댓글은 존재해야 하고, 삭제한 댓글은 프론트에서도 프레임으로 존재해야 합니다. 따라서 deleted 칼럼을 0에서 1로 변환하고 내용도 수정합니다. 기타로 프론트에 띄워지는 user 정보나 좋아요 정보는 default값으로 표시되는 작업이 CommentResponse와 ChildCommentResponse에서 수행됩니다. 그러므로 이 부분 추후 로깅 필요합니다. - CommentService(조회) : Response를 보면, ChildCommentResponse(자녀댓글), CommentResponse(부모댓글), CommentsWithCursorResponse(커서와 함께) 이렇게 3개의 Response가 있습니다. 댓글 조회 Api 명세 양식을 맞추기 위해 생성된 것입니다. Service의 조회 과정을 설명하면, postId를 바탕으로 요청이 들어오면, paginationSize에 맞는 부모댓글들이 불러와집니다. 그리고 이를 response로 변환하는 과정(getCommentsWithCursorResponse)에서 자녀댓글들 또한 response로 변환(getChildCommentResponses)됩니다. 추가로, 댓글 작성자와 현재 유저의 validation 부분이 중복된다고 생각되어 isValidate 메서드로 분리시켰고, 이를 Controller에서 호출하여 Validation 검사를 합니다.

--- orury-api/src/main/java/org/fastcampus/oruryapi/domain/comment/db/repository/CommentRepository.java
+++ orury-api/src/main/java/org/fastcampus/oruryapi/domain/comment/db/repository/CommentRepository.java
@@ -8,4 +8,10 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     int countByPostId(Long postId);
+
+    List<Comment> findByPost_IdAndParentIdAndIdGreaterThanOrderByIdAsc(Long postId, Long parentId, Long cursor, Pageable pageable);
+
+    List<Comment> findByParentIdOrderByIdAsc(Long parentId);
+
+    Comment findCommentById(Long id);
 }
