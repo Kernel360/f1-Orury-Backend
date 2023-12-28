f7997db9fe5e44ecb3c4af0ee974b693a84d3caf
GBGreenBravo <gbgreenbravo@gmail.com>
2023-12-28 22:23:16 +0900

    Refactor : Comment(댓글) Refactoring - CommentController / CommentService / CommentRepository / CommentResponse / CommentWithCursorResponse / CommentErrorCode 수정 - ChildCommentResponse 삭제

--- orury-client/src/main/java/org/fastcampus/oruryclient/comment/controller/CommentController.java
+++ orury-client/src/main/java/org/fastcampus/oruryclient/comment/controller/CommentController.java
@@ -3,18 +3,25 @@ package org.fastcampus.oruryclient.comment.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.fastcampus.oruryclient.comment.converter.response.CommentsWithCursorResponse;
-import org.fastcampus.oruryclient.comment.service.CommentService;
-import org.fastcampus.orurydomain.base.converter.ApiResponse;
 import org.fastcampus.oruryclient.comment.converter.request.CommentCreateRequest;
 import org.fastcampus.oruryclient.comment.converter.request.CommentUpdateRequest;
-import org.fastcampus.oruryclient.comment.error.CommentErrorCode;
+import org.fastcampus.oruryclient.comment.converter.response.CommentResponse;
+import org.fastcampus.oruryclient.comment.converter.response.CommentsWithCursorResponse;
+import org.fastcampus.oruryclient.comment.service.CommentLikeService;
+import org.fastcampus.oruryclient.comment.service.CommentService;
 import org.fastcampus.oruryclient.comment.util.CommentMessage;
 import org.fastcampus.oruryclient.global.constants.NumberConstants;
-import org.fastcampus.oruryclient.global.error.BusinessException;
+import org.fastcampus.oruryclient.post.service.PostService;
+import org.fastcampus.oruryclient.user.service.UserService;
+import org.fastcampus.orurydomain.base.converter.ApiResponse;
+import org.fastcampus.orurydomain.comment.dto.CommentDto;
+import org.fastcampus.orurydomain.post.dto.PostDto;
+import org.fastcampus.orurydomain.user.dto.UserDto;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -23,11 +30,18 @@ import org.springframework.web.bind.annotation.*;
 public class CommentController {
 
     private final CommentService commentService;
+    private final CommentLikeService commentLikeService;
+    private final UserService userService;
+    private final PostService postService;
 
     @Operation(summary = "댓글 생성", description = "댓글 정보를 받아, 댓글을 생성한다.")
     @PostMapping("/comment")
-    public ApiResponse<Object> createPost(@RequestBody CommentCreateRequest request) {
-        commentService.createComment(request, NumberConstants.USER_ID);
+    public ApiResponse<Object> createComment(@RequestBody CommentCreateRequest request) {
+        UserDto userDto = userService.getUserDtoById(NumberConstants.USER_ID);
+        PostDto postDto = postService.getPostDtoById(request.postId());
+        CommentDto commentDto = request.toDto(userDto, postDto);
+
+        commentService.createComment(commentDto);
 
         return ApiResponse.builder()
                 .status(HttpStatus.OK.value())
@@ -38,23 +52,34 @@ public class CommentController {
     @Operation(summary = "댓글 조회", description = "게시글 id와 cursor값을 받아, '게시글 id와 cursor값에 따른 다음 댓글 목록'과 'cursor값(목록의 마지막 댓글 id / 조회된 댓글 없다면 -1L)'을 돌려준다.")
     @GetMapping("/comments/{postId}")
     public ApiResponse<CommentsWithCursorResponse> getCommentsByPostId(@PathVariable Long postId, @RequestParam Long cursor) {
-        Long userId = NumberConstants.USER_ID;
-        CommentsWithCursorResponse responses = commentService.getCommentsByPost(userId, postId, cursor, PageRequest.of(0, NumberConstants.COMMENT_PAGINATION_SIZE));
+        PostDto postDto = postService.getPostDtoById(postId);
+        List<CommentDto> commentDtos = commentService.getParentCommentDtosByPost(postDto, cursor, PageRequest.of(0, NumberConstants.COMMENT_PAGINATION_SIZE));
+
+        List<CommentResponse> commentResponses = commentDtos.stream()
+                .map(commentDto -> {
+                    boolean isLike = commentLikeService.isLiked(NumberConstants.USER_ID, commentDto.id());
+                    return CommentResponse.of(commentDto, isLike);
+                }).toList();
+
+        CommentsWithCursorResponse response = CommentsWithCursorResponse.of(commentResponses);
 
         return ApiResponse.<CommentsWithCursorResponse>builder()
                 .status(HttpStatus.OK.value())
                 .message(CommentMessage.COMMENTS_READ.getMessage())
-                .data(responses)
+                .data(response)
                 .build();
     }
 
     @Operation(summary = "댓글 수정", description = "댓글 정보를 받아, 댓글을 수정한다.")
     @PatchMapping("/comment")
     public ApiResponse<Object> updateComment(@RequestBody CommentUpdateRequest request) {
-        Long userId = NumberConstants.USER_ID;
-        if (!commentService.isValidate(userId, request.id()))
-            throw new BusinessException(CommentErrorCode.UPDATE_FORBIDDEN);
-        commentService.updateComment(request);
+        UserDto userDto = userService.getUserDtoById(NumberConstants.USER_ID);
+        CommentDto commentDto = commentService.getCommentDtoById(request.id());
+        commentService.isValidate(commentDto, userDto);
+
+        CommentDto updatingCommentDto = request.toDto(commentDto);
+
+        commentService.updateComment(updatingCommentDto);
 
         return ApiResponse.builder()
                 .status(HttpStatus.OK.value())
@@ -65,10 +90,11 @@ public class CommentController {
     @Operation(summary = "댓글 삭제", description = "댓글 id를 받아, 댓글을 삭제한다.")
     @DeleteMapping("/comment/{id}")
     public ApiResponse<Object> deleteComment(@PathVariable Long id) {
-        Long userId = NumberConstants.USER_ID;
-        if (!commentService.isValidate(userId, id))
-            throw new BusinessException(CommentErrorCode.DELETE_FORBIDDEN);
-        commentService.deleteComment(id);
+        UserDto userDto = userService.getUserDtoById(NumberConstants.USER_ID);
+        CommentDto commentDto = commentService.getCommentDtoById(id);
+        commentService.isValidate(commentDto, userDto);
+
+        commentService.deleteComment(commentDto);
 
         return ApiResponse.builder()
                 .status(HttpStatus.OK.value())
