a899949a27c66f3b0aba1867a5a345bf46da97af
GBGreenBravo <gbgreenbravo@gmail.com>
2023-12-28 22:14:06 +0900

    Refactor : Comment 테이블에 likeCount 칼럼 추가에 따른, Entity, Dto 변경 - V1.0.1__add_viewCount_column_to_comment_table.sql 생성 - Comment / CommentDto / CommentCreateRequest / CommentUpdateRequest 수정

--- orury-client/src/main/java/org/fastcampus/oruryclient/comment/converter/request/CommentCreateRequest.java
+++ orury-client/src/main/java/org/fastcampus/oruryclient/comment/converter/request/CommentCreateRequest.java
@@ -24,6 +24,7 @@ public record CommentCreateRequest(
                 null,
                 content,
                 parentId,
+                0,
                 postDto,
                 userDto,
                 NumberConstants.IS_NOT_DELETED,
