737624143b42f482f198a8735e10bcdbe96185a8
Chan <ieejo716@naver.com>
2024-01-02 11:41:54 +0900

    Chore : Auto Closed PR test

--- orury-client/src/test/java/org/fastcampus/oruryclient/OruryClientApplicationTest.java
+++ orury-client/src/test/java/org/fastcampus/oruryclient/OruryClientApplicationTest.java
@@ -1,10 +1,9 @@
 package org.fastcampus.oruryclient;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
+//@Disabled
 @SpringBootTest
 class OruryClientApplicationTest {
     @Test
