73fc9cea332a2084a046907eacc4f9709f771fbc
Chan <ieejo716@naver.com>
2024-01-03 12:24:14 +0900

    Refactor : Flyway 수정
    Gym entity 수정

--- orury-domain/src/main/resources/db/migration/V1.0.2__create_gym.sql
+++ orury-domain/src/main/resources/db/migration/V1.0.2__create_gym.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `gym` (
-	`id`	bigint	NOT NULL,
+	`id`	bigint	NOT NULL, AUTO_INCREMENT,
 	`name`	varchar(50)	NOT NULL	COMMENT '암장 이름',
 	`road_address`	varchar(100)	NOT NULL	COMMENT '도로명주소',
 	`address`	varchar(100)	NOT NULL	COMMENT '지번 주소',
@@ -12,5 +12,8 @@ CREATE TABLE `gym` (
 	`brand`	varchar(20)	NULL,
 	`phone_number`	varchar(20)	NULL	COMMENT '암장 연락처',
 	`instagram_link`	varchar(50)	NULL,
-	`setting_day`	varchar(10)	NULL	COMMENT '세팅요일 영어로'
+	`setting_day`	varchar(10)	NULL	COMMENT '세팅요일 영어로',
+	`created_at`    DATETIME     NOT NULL COMMENT '@CreatedDate',
+    `updated_at`    DATETIME     NULL,
+	PRIMARY KEY (`id`)
 );
