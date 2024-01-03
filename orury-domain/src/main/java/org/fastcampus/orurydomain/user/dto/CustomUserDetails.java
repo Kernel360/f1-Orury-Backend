a44083f8e8c6f04ae81a0137c82091a5fb65ed32
Hyungjoon Cho <selcon1996@gmail.com>
2024-01-03 12:30:38 +0900

    Feat : OAuth2 설정 및 CustomUserDetails 생성

---
+++ orury-domain/src/main/java/org/fastcampus/orurydomain/user/dto/CustomUserDetails.java
@@ -0,0 +1,75 @@
+package org.fastcampus.orurydomain.user.dto;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public record CustomUserDetails(
+        Long id,
+        String email,
+        String nickname,
+        String password,
+        int signUpType,
+        Collection<? extends GrantedAuthority> authorities
+) implements UserDetails {
+    public static CustomUserDetails of(
+            Long id,
+            String email,
+            String nickname,
+            String password,
+            int signUpType
+    ) {
+        return CustomUserDetails.of(
+                id,
+                email,
+                nickname,
+                password,
+                signUpType
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    /**
+     * User 클래스 상속을 받지 않아 실질적으로 필요가 없다.
+     * 게정 만료 여부,
+     * 계정 lock 여부,
+     * 인증 만료 여부,
+     * 활성 여부
+     */
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
+
