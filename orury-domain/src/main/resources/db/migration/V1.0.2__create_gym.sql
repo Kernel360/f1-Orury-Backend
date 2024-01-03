8c5204b484e600f2515817d04c4c8ebf92e558a1
Chan <ieejo716@naver.com>
2024-01-03 11:09:28 +0900

    Feature : Gym Repository

---
+++ orury-domain/src/main/resources/db/migration/V1.0.2__create_gym.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `gym` (
+	`id`	bigint	NOT NULL,
+	`name`	varchar(50)	NOT NULL	COMMENT '암장 이름',
+	`road_address`	varchar(100)	NOT NULL	COMMENT '도로명주소',
+	`address`	varchar(100)	NOT NULL	COMMENT '지번 주소',
+	`score_average`	float	NULL	COMMENT '암장 평점',
+	`images`	varchar(255)	NULL	COMMENT '암장 이미지',
+	`latitude`	varchar(30)	NOT NULL	COMMENT '프론트로 보낼때는 소수점 아래 6자리까지만',
+	`longitude`	varchar(30)	NOT NULL	COMMENT '프론트로 보낼때는 소수점 아래 6자리까지만',
+	`open_time`	varchar(10)	NULL	COMMENT '최소 0000',
+	`close_time`	varchar(10)	NULL	COMMENT '최대 2400',
+	`brand`	varchar(20)	NULL,
+	`phone_number`	varchar(20)	NULL	COMMENT '암장 연락처',
+	`instagram_link`	varchar(50)	NULL,
+	`setting_day`	varchar(10)	NULL	COMMENT '세팅요일 영어로'
+);
