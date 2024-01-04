90a0d8770146cc7609fdc8fd61995c25f519dcfb
Chan <ieejo716@naver.com>
2024-01-04 13:40:39 +0900

    Chore : 패키지 이름 변경

--- orury-domain/src/main/java/org/fastcampus/orurydomain/Gym/db/repository/GymRepository.java
+++
@@ -1,8 +0,0 @@
-package org.fastcampus.orurydomain.Gym.db.repository;
-
-import org.fastcampus.orurydomain.Gym.db.model.Gym;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GymRepository extends JpaRepository<Gym, Long> {
-
-}
