695aaef562246a53f099c2e48963a507f0c04c2a
Chan <ieejo716@naver.com>
2024-01-04 20:56:25 +0900

    Feature : GymResponse
    Kakao API 호출한 데이터를 한번 파싱할 목적으로 구현

--- orury-batch/src/main/java/org/fastcampus/orurybatch/dto/GymResponse.java
+++ orury-batch/src/main/java/org/fastcampus/orurybatch/dto/GymResponse.java
@@ -4,41 +4,54 @@ import org.fastcampus.orurydomain.gym.dto.GymDto;
 
 public record GymResponse(
         String place_name,
+        String kakaoId,
         String road_address_name,
         String phone,
         String x,
         String y,
-        String address_name,
-        Boolean is_end
+        String address_name
 ) {
     public static GymResponse of(
             String place_name,
+            String kakaoId,
             String road_address_name,
             String phone,
             String x,
             String y,
-            String address_name,
-            Boolean is_end
+            String address_name
     ) {
         return new GymResponse(
                 place_name,
+                kakaoId,
                 road_address_name,
                 phone,
                 x,
                 y,
-                address_name,
-                is_end
+                address_name
+        );
+    }
+
+    public static GymResponse from(KakaoMapGymResponse.Documents docs) {
+        return GymResponse.of(
+                docs.getPlace_name(),
+                docs.getId(),
+                docs.getRoad_address_name(),
+                docs.getPhone(),
+                docs.getX(),
+                docs.getY(),
+                docs.getAddress_name()
         );
     }
 
     public GymDto toDto() {
         return GymDto.of(
                 place_name,
+                kakaoId,
                 road_address_name,
-                address_name,
-                y,
+                phone,
                 x,
-                phone
+                y,
+                address_name
         );
     }
 
