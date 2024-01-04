61b27da6d902a9250e8e9b8a60e0c73448d370aa
Chan <ieejo716@naver.com>
2024-01-04 20:56:53 +0900

    Feature : EnableScheduling
    배치 스케줄링 허용 어노테이션 작성

--- orury-batch/src/main/java/org/fastcampus/orurybatch/OruryBatchApplication.java
+++ orury-batch/src/main/java/org/fastcampus/orurybatch/OruryBatchApplication.java
@@ -3,10 +3,12 @@ package org.fastcampus.orurybatch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = {"org.fastcampus.orurybatch", "org.fastcampus.orurydomain", "org.fastcampus.orurycommon"}, exclude = FlywayAutoConfiguration.class)
 public class OruryBatchApplication {
     public static void main(String[] args) {
-        System.exit(SpringApplication.exit(SpringApplication.run(OruryBatchApplication.class, args)));
+        SpringApplication.run(OruryBatchApplication.class, args);
     }
 }
