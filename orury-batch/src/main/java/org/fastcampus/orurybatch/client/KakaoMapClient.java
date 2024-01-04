789ad89a76bff2c66aae6acda1945b3011ac983a
Chan <ieejo716@naver.com>
2024-01-04 20:51:04 +0900

    Chore : 파일 이동 및 삭제

--- orury-batch/src/main/java/org/fastcampus/orurybatch/client/KakaoMapClient.java
+++ orury-batch/src/main/java/org/fastcampus/orurybatch/client/KakaoMapClient.java
@@ -3,12 +3,13 @@ package org.fastcampus.orurybatch.job;
 import lombok.extern.slf4j.Slf4j;
 import org.fastcampus.orurybatch.dto.KakaoMapGymResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
-import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Slf4j
-@Component
+@Configuration
 public class KakaoMapClient {
     @Value("${spring.kakao.key}")
     private String env;
@@ -16,16 +17,23 @@ public class KakaoMapClient {
     @Value("${spring.kakao.baseurl}")
     private String BASE_URL;
 
-    public KakaoMapGymResponse searchGyms(String location) {
-        String queryParam = "서울시" + location + "클라이밍";
-        int page = 1;
-        log.info("queryParam: {}, page: {}", queryParam, page);
-        WebClient webClient = WebClient.builder()
-                .baseUrl(BASE_URL)
-                .defaultHeader(HttpHeaders.AUTHORIZATION, "KakaoAK " + env)
-                .build();
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
 
-        return webClient.get()
+    @Bean
+    public WebClient webClient() {
+        return webClientBuilder()
+                .baseUrl(BASE_URL)
+                .defaultHeader(HttpHeaders.AUTHORIZATION, env)
+                .build();
+    }
+
+    public KakaoMapGymResponse searchGyms(String location, int page) {
+        String queryParam = "서울시" + location + "클라이밍";
+        log.info("queryParam: {}, page: {}", queryParam, page);
+        return webClient().get()
                 .uri(uriBuilder -> uriBuilder
                         .queryParam("query", queryParam)
                         .queryParam("page", page)
