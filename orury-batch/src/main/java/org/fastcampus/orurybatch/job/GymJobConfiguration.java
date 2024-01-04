3453063afcec6bb1043eef50fe1937a99fec796f
Chan <ieejo716@naver.com>
2024-01-04 13:46:22 +0900

    Feature : GymJobConfiguration
    job, step, itemReader, itemProcessor, itemWriter로 구성되어있습니다.

--- orury-batch/src/main/java/org/fastcampus/orurybatch/job/GymJobConfiguration.java
+++ orury-batch/src/main/java/org/fastcampus/orurybatch/job/GymJobConfiguration.java
@@ -1,64 +1,83 @@
 package org.fastcampus.orurybatch.job;
 
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.fastcampus.orurybatch.config.JobCompletionNotificationListener;
+import org.fastcampus.orurybatch.dto.KakaoMapGymResponse;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Slf4j
 @RequiredArgsConstructor
 @Configuration
 public class GymJobConfiguration {
+    private final KakaoMapClient kakaoMapClient;
 
-//  todo  private final GymRepository gymRepository;
+    @Bean
+    public Job job(
+            JobRepository jobRepository,
+            JobCompletionNotificationListener listener,
+            Step step
+    ) {
+        return new JobBuilder("job", jobRepository)
+                .listener(listener)
+                .start(step)
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
 
-//    @Bean
-//    public Job job(
-//            JobRepository jobRepository,
-//            JobCompletionNotificationListener listener,
-//            Step step
-//    ) {
-//        return new JobBuilder("job", jobRepository)
-//                .listener(listener)
-//                .start(step)
-//                .incrementer(new RunIdIncrementer())
-//                .build();
-//    }
-//
-//    @Bean
-//    public Step step(
-//            JobRepository jobRepository,
-//            PlatformTransactionManager transactionManager,
-//            KakaoResponseItemReader<KakaoResponse> itemReader,
+    @Bean
+    public Step step(
+            JobRepository jobRepository,
+            PlatformTransactionManager transactionManager,
+            GymItemReader itemReader,
 //            GymItemProcessor itemProcessor,
-//            JdbcBatchItemWriter<Gym> itemWriter,
-//            ) {
-//        return new StepBuilder("step", jobRepository)
-//                .<KakaoResponse, Gym>chunk(15, transactionManager)
-//                .reader(itemReader)
+            ItemWriter<KakaoMapGymResponse> itemWriter
+    ) {
+        return new StepBuilder("step", jobRepository)
+                .<KakaoMapGymResponse, KakaoMapGymResponse>chunk(1, transactionManager)
+                .reader(itemReader)
 //                .processor(itemProcessor)
-//                .writer(itemWriter)
-//                .build();
-//    }
-//
-//    @Bean
-//    public ItemReader<KakaoResponse> itemReader() {
-//        return new KakaoResponseItemReader();
-//    }
-//
+                .writer(itemWriter)
+                .build();
+    }
+
+    @Bean
+    public GymItemReader itemReader() {
+        log.info("itemReader()");
+        return new GymItemReader(kakaoMapClient);
+    }
+
 //    @Bean
 //    public GymItemProcessor itemProcessor() {
 //        return new GymItemProcessor();
 //    }
-//
+
+    @Bean
+    public ItemWriter<KakaoMapGymResponse> itemWriter() {
+        return items -> {
+            for (KakaoMapGymResponse item : items) {
+                item.getDocuments().forEach(doc -> {
+                    log.info("item = {}", doc.getPlace_name());
+                });
+            }
+        };
+    }
 //    @Bean
-//    public JdbcBatchItemWriter<Gym> itemWriter(DataSource dataSource) {
-//        return new JdbcBatchItemWriterBuilder<>()
-//                .sql()
-//                .dataSource(dataSource)
-//                .beanMapped()
+//    public JpaItemWriter<List<GymDto>> itemWriter(
+//            EntityManagerFactory entityManagerFactory
+//    ) throws Exception {
+//        return new JpaItemWriterBuilder<List<GymDto>>()
+//                .entityManagerFactory(entityManagerFactory)
 //                .build();
 //    }
-
 
 }
