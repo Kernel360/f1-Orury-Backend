413bf41a6b4c76860ac890cf00250c9169854430
Hyungjoon Cho <selcon1996@gmail.com>
2024-01-04 16:30:28 +0900

    Refactor : sign-in -> login 네이밍 변경

--- orury-domain/src/main/java/org/fastcampus/orurydomain/user/db/repository/UserRepository.java
+++ orury-domain/src/main/java/org/fastcampus/orurydomain/user/db/repository/UserRepository.java
@@ -3,8 +3,10 @@ package org.fastcampus.orurydomain.user.db.repository;
 import org.fastcampus.orurydomain.user.db.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findUserById(Long id);
 
-    User findByEmailAndSignUpType(String email, int signUpType);
+    Optional<User> findByEmailAndSignUpType(String email, int signUpType);
 }
