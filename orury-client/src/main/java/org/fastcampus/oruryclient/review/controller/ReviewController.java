2c1f3ee4134b9b802c6b7b162379b6d67cb20e01
Yejin Song <yjyjsssong@gmail.com>
2024-01-08 20:04:06 +0900

    Refactor : NumberConstants.USER_ID에 대한 검증 로직 삭제

--- orury-client/src/main/java/org/fastcampus/oruryclient/review/controller/ReviewController.java
+++ orury-client/src/main/java/org/fastcampus/oruryclient/review/controller/ReviewController.java
@@ -1,8 +1,5 @@
 package org.fastcampus.oruryclient.review.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.fastcampus.oruryclient.global.constants.NumberConstants;
 import org.fastcampus.oruryclient.gym.service.GymService;
 import org.fastcampus.oruryclient.review.converter.request.ReviewCreateRequest;
@@ -19,9 +16,21 @@ import org.fastcampus.orurydomain.review.dto.ReviewDto;
 import org.fastcampus.orurydomain.user.dto.UserDto;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -71,9 +80,7 @@ public class ReviewController {
     public ApiResponse<Object> updateReview(@PathVariable Long reviewId, @RequestBody ReviewUpdateRequest request) {
         ReviewDto beforeReviewDto = reviewService.getReviewDtoById(reviewId);
 
-        UserDto userDto = userService.getUserDtoById(NumberConstants.USER_ID);
-
-        reviewService.isValidate(beforeReviewDto.userDto().id(), userDto.id());
+        reviewService.isValidate(beforeReviewDto.userDto().id(), NumberConstants.USER_ID);
 
         ReviewDto updateReviewDto = request.toDto(beforeReviewDto);
         reviewService.updateReview(updateReviewDto);
@@ -87,10 +94,9 @@ public class ReviewController {
     @Operation(summary = "리뷰 삭제", description = "리뷰 id를 받아, 리뷰를 삭제한다. ")
     @DeleteMapping("/review/{reviewId}")
     public ApiResponse<Object> deleteReview(@PathVariable Long reviewId) {
-        UserDto userDto = userService.getUserDtoById(NumberConstants.USER_ID);
         ReviewDto reviewDto = reviewService.getReviewDtoById(reviewId);
 
-        reviewService.isValidate(userDto.id(), reviewDto.userDto().id());
+        reviewService.isValidate(NumberConstants.USER_ID, reviewDto.userDto().id());
 
         reviewService.deleteReview(reviewDto);
 
