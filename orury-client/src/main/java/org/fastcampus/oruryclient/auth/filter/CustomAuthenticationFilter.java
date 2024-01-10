94e88c5839d927e795ad38be959aac3f79e5c83e
Hyungjoon Cho <selcon1996@gmail.com>
2024-01-10 12:49:09 +0900

    Refacor : CustomAuthenticationFilter 수정
    - email만 받아와서 로그인 되도록 수정
    - 패스워드는 signUpType을 encrypt한 값으로 대체

--- orury-client/src/main/java/org/fastcampus/oruryclient/auth/filter/CustomAuthenticationFilter.java
+++ orury-client/src/main/java/org/fastcampus/oruryclient/auth/filter/CustomAuthenticationFilter.java
@@ -1,46 +1,59 @@
 package org.fastcampus.oruryclient.auth.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.fastcampus.oruryclient.auth.converter.request.LoginRequest;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.io.IOException;
+import java.util.Collections;
+
 @Slf4j
-@RequiredArgsConstructor
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    private final AuthenticationManager authenticationManager;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super();
+        setAuthenticationManager(authenticationManager);
+        setFilterProcessesUrl("/auth/login");
+    }
 
     @Override
     public Authentication attemptAuthentication(
             HttpServletRequest request,
             HttpServletResponse response
-    ) {
-        String username = obtainUsername(request);
-        String password = obtainUsername(request);
+    ) throws AuthenticationException {
+        try {
+            LoginRequest loginRequest = objectMapper.readValue(request.getInputStream(), LoginRequest.class);
 
-        System.out.println(username);
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    loginRequest.email(), loginRequest.signUpType(), Collections.emptyList()
+            );
 
-        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password, null);
-
-        return authenticationManager.authenticate(authToken);
+            return getAuthenticationManager().authenticate(authToken);
+        } catch (IOException e) {
+            throw new AuthenticationServiceException("Failed to parse authentication request body", e);
+        }
     }
 
     //로그인 성공시 실행하는 메소드 (여기서 JWT를 발급하면 됨)
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
-
+        System.out.println("로그인 성공!!!!!!!!!!!");
     }
 
     //로그인 실패시 실행하는 메소드
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
-
+        System.out.println("실패패패패퍂패ㅐ");
     }
 }
