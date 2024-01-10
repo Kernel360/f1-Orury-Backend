21f5664c6bbea67dad6e07e26b9437bf66d0386e
Chan <ieejo716@naver.com>
2024-01-10 11:27:26 +0900

    Refactor : GlobalExceptionHandler
    - BusinessException : 비즈니스 로직과 관련된 예외사항을 처리
    - AuthException : 인증, 인가와 관련된 예외사항을 처리
    - Exception : Handler에서 잡아내지 못한 예외사항 감지

--- orury-common/src/main/java/org/fastcampus/orurycommon/error/exception/GlobalExceptionHandler.java
+++ orury-common/src/main/java/org/fastcampus/orurycommon/error/exception/GlobalExceptionHandler.java
@@ -1,14 +1,39 @@
 package org.fastcampus.orurycommon.error.exception;
 
-import org.fastcampus.orurycommon.error.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.fastcampus.orurycommon.error.code.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    //비즈니스 로직 예외 처리 -> BadRequest의 역할
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
-        return ResponseEntity.status(e.getStatus()).body(ErrorResponse.of(e.getStatus(), e.getMessage()));
+        log.warn(e.getMessage(), e);
+        return ResponseEntity
+                .status(e.getStatus())
+                .body(ErrorResponse.of(e.getStatus(), e.getMessage()));
+    }
+
+    //인증 예외 처리 -> Unauthorized의 역할
+    @ExceptionHandler(AuthException.class)
+    public ResponseEntity<ErrorResponse> handleAuthException(AuthException e) {
+        log.warn(e.getMessage(), e);
+        return ResponseEntity
+                .status(e.getStatus())
+                .body(ErrorResponse.of(e.getStatus(), e.getMessage()));
+    }
+
+    //그 외 모든 예외 처리 -> InternalServerError의 역할
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.warn(e.getMessage(), e);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), "unhandled exception"));
     }
 }
