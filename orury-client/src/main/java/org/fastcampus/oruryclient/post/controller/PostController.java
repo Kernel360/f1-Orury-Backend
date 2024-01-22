552c7eb18dbae688c908879456ac4f0e52c0db39
Chan <ieejo716@naver.com>
2024-01-22 23:50:37 +0900

    Refactor : PostController Multipart apply
    require option false 인 MultiPartFile들을 추가한 컨트롤러

--- orury-client/src/main/java/org/fastcampus/oruryclient/post/controller/PostController.java
+++ orury-client/src/main/java/org/fastcampus/oruryclient/post/controller/PostController.java
@@ -23,12 +23,13 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/api/v1/post")
 @RestController
 public class PostController {
     private final PostService postService;
@@ -36,12 +37,16 @@ public class PostController {
     private final PostLikeService postLikeService;
 
     @Operation(summary = "게시글 생성", description = "게시글 정보를 받아, 게시글을 생성한다.")
-    @PostMapping("/post")
-    public ApiResponse<Object> createPost(@RequestBody PostCreateRequest request, @AuthenticationPrincipal UserPrincipal userPrincipal) {
+    @PostMapping
+    public ApiResponse<Object> createPost(
+            @RequestPart PostCreateRequest request,
+            @RequestPart(required = false) MultipartFile[] images,
+            @AuthenticationPrincipal UserPrincipal userPrincipal
+    ) {
         UserDto userDto = userService.getUserDtoById(userPrincipal.id());
         PostDto postDto = request.toDto(userDto);
 
-        postService.createPost(postDto);
+        postService.createPost(postDto, images);
 
         return ApiResponse.builder()
                 .status(HttpStatus.OK.value())
@@ -50,14 +55,14 @@ public class PostController {
     }
 
     @Operation(summary = "게시글 상세 조회", description = "게시글 id를 받아, 게시글 상세 정보를 돌려준다.")
-    @GetMapping("/post/{id}")
+    @GetMapping("/{id}")
     public ApiResponse<PostResponse> getPostById(@PathVariable Long id, @AuthenticationPrincipal UserPrincipal userPrincipal) {
         PostDto postDto = postService.getPostDtoById(id);
-
+        UserDto userDto = userService.getUserDtoById(userPrincipal.id());
         postService.addViewCount(postDto);
 
         boolean isLike = postLikeService.isLiked(userPrincipal.id(), id);
-        PostResponse response = PostResponse.of(postDto, userPrincipal.id(), isLike);
+        PostResponse response = PostResponse.of(postDto, userDto, isLike);
 
         return ApiResponse.<PostResponse>builder()
                 .status(HttpStatus.OK.value())
@@ -67,11 +72,12 @@ public class PostController {
     }
 
     @Operation(summary = "카테고리별 게시글 목록 조회", description = "카테고리(1: 자유게시판, 2: Q&A게시판)와 cursor값을 받아, '카테고리와 cursor값에 따른 다음 게시글 목록'과 'cursor값(목록의 마지막 게시글 id / 조회된 게시글 없다면 -1L)'을 돌려준다.")
-    @GetMapping("/posts/{category}")
+    @GetMapping("/list/{category}")
     public ApiResponse<PostsWithCursorResponse> getPostsByCategory(@PathVariable int category, @RequestParam Long cursor) {
         List<PostDto> postDtos = postService.getPostDtosByCategory(category, cursor, PageRequest.of(0, NumberConstants.POST_PAGINATION_SIZE));
         List<PostsResponse> postsResponses = postDtos.stream()
-                .map(PostsResponse::of).toList();
+                .map(PostsResponse::of)
+                .toList();
 
         PostsWithCursorResponse response = PostsWithCursorResponse.of(postsResponses, cursor);
 
@@ -83,11 +89,12 @@ public class PostController {
     }
 
     @Operation(summary = "검색어에 따른 게시글 목록 조회", description = "검색어와 cursor값을 받아, '검색어와 cursor값에 따른 다음 게시글 목록'과 'cursor값(목록의 마지막 게시글 id / 조회된 게시글 없다면 -1L)'을 돌려준다.")
-    @GetMapping("/posts")
+    @GetMapping("/list")
     public ApiResponse<PostsWithCursorResponse> getPostsBySearchWord(@RequestParam String searchWord, @RequestParam Long cursor) {
         List<PostDto> postDtos = postService.getPostDtosBySearchWord(searchWord, cursor, PageRequest.of(0, NumberConstants.POST_PAGINATION_SIZE));
         List<PostsResponse> postsResponses = postDtos.stream()
-                .map(PostsResponse::of).toList();
+                .map(PostsResponse::of)
+                .toList();
 
         PostsWithCursorResponse response = PostsWithCursorResponse.of(postsResponses, cursor);
 
@@ -99,11 +106,12 @@ public class PostController {
     }
 
     @Operation(summary = "인기 게시글 목록 조회", description = "page값을 받아, 'page번호에 따른 인기 게시글 목록'과 'page값(다음으로 조회할 page 번호 / 현재 마지막 페이지를 반환한다면 -1)'을 돌려준다.")
-    @GetMapping("/posts/hot")
+    @GetMapping("/list/hot")
     public ApiResponse<PostsWithPageResponse> getHotPosts(@RequestParam int page) {
         Page<PostDto> postDtos = postService.getHotPostDtos(PageRequest.of(page, NumberConstants.POST_PAGINATION_SIZE));
         List<PostsResponse> postsResponses = postDtos.stream()
-                .map(PostsResponse::of).toList();
+                .map(PostsResponse::of)
+                .toList();
 
         int nextPage = postService.getNextPage(postDtos, page);
         PostsWithPageResponse response = PostsWithPageResponse.of(postsResponses, nextPage);
@@ -116,15 +124,19 @@ public class PostController {
     }
 
     @Operation(summary = "게시글 수정", description = "게시글 정보를 받아, 게시글을 수정한다.")
-    @PatchMapping("/post")
-    public ApiResponse<Object> updatePost(@RequestBody PostUpdateRequest request, @AuthenticationPrincipal UserPrincipal userPrincipal) {
+    @PatchMapping
+    public ApiResponse<Object> updatePost(
+            @RequestPart PostUpdateRequest request,
+            @RequestPart(required = false) MultipartFile[] images,
+            @AuthenticationPrincipal UserPrincipal userPrincipal
+    ) {
         UserDto userDto = userService.getUserDtoById(userPrincipal.id());
         PostDto postDto = postService.getPostDtoById(request.id());
         postService.isValidate(postDto, userDto);
 
         PostDto updatingPostDto = request.toDto(postDto, userDto);
 
-        postService.updatePost(updatingPostDto);
+        postService.updatePost(updatingPostDto, images);
 
         return ApiResponse.builder()
                 .status(HttpStatus.OK.value())
@@ -133,7 +145,7 @@ public class PostController {
     }
 
     @Operation(summary = "게시글 삭제", description = "게시글 id를 받아, 게시글을 삭제한다.")
-    @DeleteMapping("/post/{id}")
+    @DeleteMapping("/{id}")
     public ApiResponse<Object> deletePost(@PathVariable Long id, @AuthenticationPrincipal UserPrincipal userPrincipal) {
         UserDto userDto = userService.getUserDtoById(userPrincipal.id());
         PostDto postDto = postService.getPostDtoById(id);
