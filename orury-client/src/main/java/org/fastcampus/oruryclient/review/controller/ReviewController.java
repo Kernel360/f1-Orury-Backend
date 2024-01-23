64f05abf2c6cba870c91cb3004b78cba4c9c596b
Chan <ieejo716@naver.com>
2024-01-23 11:37:45 +0900

    Refactor : ReviewController
    RequestBody -> RequestParts로 나뉘고 MultipartFile로 받는 부분 수정

--- orury-client/src/main/java/org/fastcampus/oruryclient/review/controller/ReviewController.java
+++ orury-client/src/main/java/org/fastcampus/oruryclient/review/controller/ReviewController.java
@@ -23,12 +23,13 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/api/v1/review")
 @RestController
 public class ReviewController {
     private final ReviewService reviewService;
@@ -37,8 +38,12 @@ public class ReviewController {
     private final GymService gymService;
 
     @Operation(summary = "리뷰 생성", description = "requestbody로 리뷰 정보를 받아, 리뷰를 생성한다.")
-    @PostMapping("/review")
-    public ApiResponse<Object> createReview(@RequestBody ReviewCreateRequest request, @AuthenticationPrincipal UserPrincipal userPrincipal) {
+    @PostMapping
+    public ApiResponse<Object> createReview(
+            @RequestPart ReviewCreateRequest request,
+            @RequestPart(required = false) MultipartFile[] images,
+            @AuthenticationPrincipal UserPrincipal userPrincipal
+    ) {
         UserDto userDto = userService.getUserDtoById(userPrincipal.id());
         GymDto gymDto = gymService.getGymDtoById(request.gymId());
 
@@ -46,7 +51,7 @@ public class ReviewController {
 
         ReviewDto reviewDto = request.toDto(userDto, gymDto);
 
-        reviewService.createReview(reviewDto);
+        reviewService.createReview(reviewDto, images);
 
         return ApiResponse.builder()
                 .status(HttpStatus.OK.value())
@@ -56,7 +61,7 @@ public class ReviewController {
     }
 
     @Operation(summary = "리뷰 조회", description = "리뷰 수정을 위해 리뷰 id로 기존에 있는 값을 조회하여 정보를 가져온다.")
-    @GetMapping("/review/{reviewId}")
+    @GetMapping("/{reviewId}")
     public ApiResponse<Object> getReview(@PathVariable Long reviewId, @AuthenticationPrincipal UserPrincipal userPrincipal) {
 
         ReviewDto reviewDto = reviewService.getReviewDtoById(reviewId);
@@ -71,14 +76,20 @@ public class ReviewController {
     }
 
     @Operation(summary = "리뷰 수정", description = "기존 리뷰를 불러온 후, 수정할 리뷰 정보를 받아, 리뷰를 수정한다.")
-    @PatchMapping("/review/{reviewId}")
-    public ApiResponse<Object> updateReview(@PathVariable Long reviewId, @RequestBody ReviewUpdateRequest request, @AuthenticationPrincipal UserPrincipal userPrincipal) {
+    @PatchMapping("/{reviewId}")
+    public ApiResponse<Object> updateReview(
+            @PathVariable Long reviewId,
+            @RequestPart ReviewUpdateRequest request,
+            @RequestPart(required = false) MultipartFile[] images,
+            @AuthenticationPrincipal UserPrincipal userPrincipal
+    ) {
         ReviewDto beforeReviewDto = reviewService.getReviewDtoById(reviewId);
 
-        reviewService.isValidate(beforeReviewDto.userDto().id(), userPrincipal.id());
+        reviewService.isValidate(beforeReviewDto.userDto()
+                .id(), userPrincipal.id());
 
         ReviewDto updateReviewDto = request.toDto(beforeReviewDto);
-        reviewService.updateReview(updateReviewDto);
+        reviewService.updateReview(updateReviewDto, images);
 
         return ApiResponse.builder()
                 .status(HttpStatus.OK.value())
@@ -87,11 +98,12 @@ public class ReviewController {
     }
 
     @Operation(summary = "리뷰 삭제", description = "리뷰 id를 받아, 리뷰를 삭제한다.")
-    @DeleteMapping("/review/{reviewId}")
+    @DeleteMapping("/{reviewId}")
     public ApiResponse<Object> deleteReview(@PathVariable Long reviewId, @AuthenticationPrincipal UserPrincipal userPrincipal) {
         ReviewDto reviewDto = reviewService.getReviewDtoById(reviewId);
 
-        reviewService.isValidate(userPrincipal.id(), reviewDto.userDto().id());
+        reviewService.isValidate(userPrincipal.id(), reviewDto.userDto()
+                .id());
 
         reviewService.deleteReview(reviewDto);
 
@@ -102,7 +114,7 @@ public class ReviewController {
     }
 
     @Operation(summary = "암장 별 리뷰 조회", description = "암장 id를 받아 해당 암장의 리뷰를 반환한다.")
-    @GetMapping("/reviews/{gymId}")
+    @GetMapping("/gym/{gymId}")
     public ApiResponse<Object> getReviews(@PathVariable Long gymId, @RequestParam Long cursor, @AuthenticationPrincipal UserPrincipal userPrincipal) {
         List<ReviewDto> reviewDtos = reviewService.getReviewDtosByGymId(gymId, cursor, PageRequest.of(0, NumberConstants.REVIEW_PAGINATION_SIZE));
         List<ReviewsResponse> reviewsResponses = reviewDtos.stream()
@@ -112,7 +124,8 @@ public class ReviewController {
                 })
                 .toList();
 
-        String gymName = gymService.getGymDtoById(gymId).name();
+        String gymName = gymService.getGymDtoById(gymId)
+                .name();
 
         ReviewsWithCursorResponse response = ReviewsWithCursorResponse.of(reviewsResponses, gymName);
 
