962e76a056c8e2180cc86f9089f0e2e116146dbe
GBGreenBravo <gbgreenbravo@gmail.com>
2024-01-23 15:56:55 +0900

    refactor : PR review 반영 (구분자 ~에서 -로 변경)

--- orury-common/src/main/java/org/fastcampus/orurycommon/util/BusinessHoursConverter.java
+++ orury-common/src/main/java/org/fastcampus/orurycommon/util/BusinessHoursConverter.java
@@ -8,14 +8,14 @@ import java.time.LocalTime;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class BusinessHoursConverter {
     public static LocalTime extractOpenTime(String businessHour) {
-        String openTime = businessHour.split("~")[0];
+        String openTime = businessHour.split("-")[0];
         int openHour = Integer.parseInt(openTime.split(":")[0]);
         int openMinute = Integer.parseInt(openTime.split(":")[1]);
         return LocalTime.of(openHour, openMinute);
     }
 
     public static LocalTime extractCloseTime(String businessHour) {
-        String closeTime = businessHour.split("~")[1];
+        String closeTime = businessHour.split("-")[1];
         int closeHour = Integer.parseInt(closeTime.split(":")[0]);
         int closeMinute = Integer.parseInt(closeTime.split(":")[1]);
         return LocalTime.of(closeHour, closeMinute);
