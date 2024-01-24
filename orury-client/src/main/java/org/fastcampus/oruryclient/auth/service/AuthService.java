88df9b3bd4f5c004dbc954558229b793399d253f
Hyungjoon Cho <selcon1996@gmail.com>
2024-01-24 12:39:02 +0900

    Feat : 카카오 토큰 발급 로직 생성

--- orury-client/src/main/java/org/fastcampus/oruryclient/auth/service/AuthService.java
+++ orury-client/src/main/java/org/fastcampus/oruryclient/auth/service/AuthService.java
@@ -1,24 +1,41 @@
 package org.fastcampus.oruryclient.auth.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import org.fastcampus.orurycommon.error.code.UserErrorCode;
 import org.fastcampus.orurycommon.error.exception.BusinessException;
+import org.fastcampus.orurydomain.auth.dto.KakaoTokenDto;
 import org.fastcampus.orurydomain.user.db.repository.UserRepository;
 import org.fastcampus.orurydomain.user.dto.UserDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 @RequiredArgsConstructor
 @Service
 public class AuthService {
     private final UserRepository userRepository;
 
-    /**
-     * 회원가입
-     *
-     * @param userDto
-     */
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+    String kakaoClientId;
+
+    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
+    String kakaoRedirectURI;
+
+    @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
+    String kakaoClientSecret;
+
     @Transactional
     public void signUp(UserDto userDto) {
         try {
@@ -27,4 +44,47 @@ public class AuthService {
             throw new BusinessException(UserErrorCode.DUPLICATED_USER);
         }
     }
+
+    @Transactional
+    public KakaoTokenDto getKakaoAccessToken(String code) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+
+        // Http Response Body 객체 생성
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code"); //카카오 공식문서 기준 authorization_code 로 고정
+        params.add("client_id", kakaoClientId); // 카카오 Dev 앱 REST API 키
+        params.add("redirect_uri", kakaoRedirectURI); // 카카오 Dev redirect uri
+        params.add("code", code); // 프론트에서 인가 코드 요청시 받은 인가 코드값
+        params.add("client_secret", kakaoClientSecret); // 카카오 Dev 카카오 로그인 Client Secret
+
+        // 헤더와 바디 합치기 위해 Http Entity 객체 생성
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+
+        // 카카오로부터 Access token 받아오기
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> accessTokenResponse = rt.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        // JSON Parsing (-> KakaoTokenDto)
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        KakaoTokenDto kakaoTokenDto = null;
+        try {
+            kakaoTokenDto = objectMapper.readValue(accessTokenResponse.getBody(), KakaoTokenDto.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return kakaoTokenDto;
+    }
+
+    
 }
