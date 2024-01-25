742ad53bfd20dc33978b399009c65ecee407ad29
Yejin Song <yjyjsssong@gmail.com>
2024-01-25 15:40:47 +0900

    feat : 리뷰 생성, 수정, 삭제에 따른 count, score 변동 구현 리뷰 생성, 수정, 삭제에 따라 gym의 review_count, total_score가 함께 바뀔 수 있도록 구현

--- orury-domain/src/main/java/org/fastcampus/orurydomain/gym/db/repository/GymRepository.java
+++ orury-domain/src/main/java/org/fastcampus/orurydomain/gym/db/repository/GymRepository.java
@@ -4,6 +4,7 @@ import org.fastcampus.orurydomain.gym.db.model.Gym;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -17,4 +18,22 @@ public interface GymRepository extends JpaRepository<Gym, Long> {
     @Modifying
     @Query("UPDATE gym SET likeCount = likeCount - 1 WHERE id = :gymId")
     void decreaseLikeCount(Long gymId);
+
+    @Modifying
+    @Query("UPDATE gym SET reviewCount = reviewCount + 1 WHERE id = :gymId")
+    void increaseReviewCount(Long gymId);
+
+    @Modifying
+    @Query("UPDATE gym SET reviewCount = reviewCount - 1 WHERE id = :gymId")
+    void decreaseReviewCount(Long gymId);
+
+    @Modifying
+    @Query("UPDATE gym SET totalScore = totalScore + :reviewScore WHERE id = :gymId")
+    void sumTotalScore(@Param("gymId") Long gymId, @Param("reviewScore") float reviewScore);
+
+    @Modifying
+    @Query("UPDATE gym SET totalScore = totalScore - :reviewScore WHERE id = :gymId")
+    void subtractTotalScore(@Param("gymId") Long gymId, @Param("reviewScore") float reviewScore);
+
+
 }
