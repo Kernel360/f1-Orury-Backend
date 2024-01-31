101101e43bf1c7745511188580aa465240d01cde
Hyungjoon Cho <selcon1996@gmail.com>
2024-01-31 18:31:35 +0900

    Chore : 메세지 문구 수정

--- orury-client/src/main/java/org/fastcampus/oruryclient/comment/converter/message/CommentMessage.java
+++ orury-client/src/main/java/org/fastcampus/oruryclient/comment/converter/message/CommentMessage.java
@@ -4,13 +4,13 @@ import lombok.Getter;
 
 @Getter
 public enum CommentMessage {
-    COMMENT_CREATED("댓글이 정상적으로 작성되었습니다."),
-    COMMENT_READ("댓글이 정상적으로 조회되었습니다."),
-    COMMENTS_READ("댓글 목록을 정상적으로 조회했습니다."),
-    COMMENT_UPDATED("댓글 수정이 완료되었습니다."),
-    COMMENT_DELETED("댓글 삭제가 완료되었습니다."),
-    COMMENT_LIKE_CREATED("댓글 좋아요가 완료되었습니다."),
-    COMMENT_LIKE_DELETED("댓글 좋아요를 취소했습니다.");
+    COMMENT_CREATED("댓글이 작성되었습니다."),
+    COMMENT_READ("댓글이 조회되었습니다."),
+    COMMENTS_READ("댓글 목록이 조회되었습니다."),
+    COMMENT_UPDATED("댓글이 수정되었습니다."),
+    COMMENT_DELETED("댓글이 삭제되었습니다."),
+    COMMENT_LIKE_CREATED("댓글 좋아요가 생성되었습니다."),
+    COMMENT_LIKE_DELETED("댓글 좋아요가 삭제되었습니다.");
 
     private final String message;
 
--- orury-client/src/main/java/org/fastcampus/oruryclient/post/converter/message/PostMessage.java
+++ orury-client/src/main/java/org/fastcampus/oruryclient/post/converter/message/PostMessage.java
@@ -4,13 +4,13 @@ import lombok.Getter;
 
 @Getter
 public enum PostMessage {
-    POST_CREATED("게시글이 정상적으로 작성되었습니다."),
-    POST_READ("게시글을 정상적으로 조회했습니다."),
-    POSTS_READ("게시글 카테고리를 정상적으로 조회했습니다."),
-    POST_UPDATED("게시글 수정이 완료되었습니다."),
-    POST_DELETED("게시글 삭제가 완료되었습니다."),
-    POST_LIKE_CREATED("게시글 좋아요가 완료되었습니다."),
-    POST_LIKE_DELETED("게시글 좋아요를 취소했습니다.");
+    POST_CREATED("게시글이 작성되었습니다."),
+    POST_READ("게시글이 조회되었습니다."),
+    POSTS_READ("게시글 목록이 조회되었습니다."),
+    POST_UPDATED("게시글이 수정되었습니다."),
+    POST_DELETED("게시글이 삭제되었습니다."),
+    POST_LIKE_CREATED("게시글 좋아요가 생성되었습니다."),
+    POST_LIKE_DELETED("게시글 좋아요가 삭제되었습니다.");
 
     private final String message;
 
