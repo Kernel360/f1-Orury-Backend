22bfc8ba3dffd3219782f5e319ac086c7d6f7a6f
GBGreenBravo <gbgreenbravo@gmail.com>
2024-01-31 13:26:48 +0900

    refactor : LogAspect 메서드 병합

--- orury-common/src/main/java/org/fastcampus/orurycommon/log/LogAspect.java
+++ orury-common/src/main/java/org/fastcampus/orurycommon/log/LogAspect.java
@@ -1,9 +1,7 @@
 package org.fastcampus.orurycommon.log;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
@@ -19,17 +17,13 @@ public class LogAspect {
         Object objects = proceedingJoinPoint.getArgs();
         log.info("### Method Start : {} , Parameters : {} ", methodSignature, objects);
 
-        Object res = proceedingJoinPoint.proceed();
-        log.info("### Method End : {} , Result : {} ", methodSignature, res);
-
-        return res;
-    }
-
-    @AfterThrowing(value = "within(@org.springframework.stereotype.Service *)", throwing = "exception")
-    public void loggingAfterServiceFail(JoinPoint joinPoint, Exception exception) {
-        String methodSignature = joinPoint.getSignature().toShortString();
-        Object objects = joinPoint.getArgs();
-
-        log.error("### Error Occurred : {} , Method : {}, Parameters : {}", exception.getMessage(), methodSignature, objects, exception);
+        try {
+            Object res = proceedingJoinPoint.proceed();
+            log.info("### Method End : {} , Result : {} ", methodSignature, res);
+            return res;
+        } catch (Throwable throwable) {
+            log.error("### Error Occurred in Method : {} , Message : {} ", methodSignature, throwable.getMessage(), throwable);
+            throw throwable;
+        }
     }
 }
