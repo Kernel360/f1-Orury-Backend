3685b4193735f0642fa23b2cebc84c389519a828
GBGreenBravo <gbgreenbravo@gmail.com>
2024-02-01 18:37:34 +0900

    feat : User 탈퇴 구현

--- orury-domain/src/main/java/org/fastcampus/orurydomain/gym/db/repository/GymLikeRepository.java
+++ orury-domain/src/main/java/org/fastcampus/orurydomain/gym/db/repository/GymLikeRepository.java
@@ -4,6 +4,10 @@ import org.fastcampus.orurydomain.gym.db.model.GymLike;
 import org.fastcampus.orurydomain.gym.db.model.GymLikePK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GymLikeRepository extends JpaRepository<GymLike, GymLikePK> {
     boolean existsByGymLikePK_UserIdAndGymLikePK_GymId(Long userId, Long gymId);
+
+    List<GymLike> findByGymLikePK_UserId(Long userId);
 }
