ad8adca75a3dece4f8873a319ecbb3cda5086a52
YunNote <zzdd1558@gmail.com>
2024-02-12 12:51:47 +0900

    feature: 과도한 줄바꿈이 오히려 가독성이 떨어질 수 있음. getPassword()의 반환이 email인 이유 ?

--- orury-admin/src/main/java/org/fastcampus/oruryadmin/OruryAdminApplication.java
+++ orury-admin/src/main/java/org/fastcampus/oruryadmin/OruryAdminApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 @SpringBootApplication(scanBasePackages = {"org.fastcampus.orurycommon", "org.fastcampus.orurydomain", "org.fastcampus.oruryadmin"}, exclude = FlywayAutoConfiguration.class)
 public class OruryAdminApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(OruryAdminApplication.class, args);
     }
 }
--- orury-domain/src/main/java/org/fastcampus/orurydomain/user/dto/UserPrincipal.java
+++ orury-domain/src/main/java/org/fastcampus/orurydomain/user/dto/UserPrincipal.java
@@ -9,75 +9,70 @@ import java.util.Collections;
 import java.util.List;
 
 public record UserPrincipal(
-        Long id,
-        String email,
-        Collection<? extends GrantedAuthority> authorities
+   Long id,
+   String email,
+   Collection<? extends GrantedAuthority> authorities
 ) implements UserDetails {
-    public static UserPrincipal of(
-            Long id,
-            String email,
-            Collection<? extends GrantedAuthority> authorities
-    ) {
-        return new UserPrincipal(
-                id,
-                email,
-                authorities
-        );
-    }
 
-    public static UserPrincipal fromToken(Long id, String email, String role) {
-        // SimpleGrantedAuthority를 사용하여 주어진 role로 권한 리스트를 생성합니다.
-        List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority(role));
+   //
+   public static UserPrincipal of(
+      Long id,
+      String email,
+      Collection<? extends GrantedAuthority> authorities
+   ) {
+      return new UserPrincipal(
+         id,
+         email,
+         authorities
+      );
+   }
 
-        // 생성자의 마지막 인자로 authorities를 전달하여 UserPrincipal 인스턴스를 생성합니다.
-        return UserPrincipal.of(
-                id,
-                email,
-                authorities // 생성한 권한 리스트
-        );
-    }
+   public static UserPrincipal fromToken(Long id, String email, String role) {
+      // SimpleGrantedAuthority를 사용하여 주어진 role로 권한 리스트를 생성합니다.
+      List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority(role));
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
+      // 생성자의 마지막 인자로 authorities를 전달하여 UserPrincipal 인스턴스를 생성합니다.
+      return UserPrincipal.of(id, email, authorities);
+   }
 
-    @Override
-    public String getPassword() {
-        return email;
-    }
+   @Override
+   public Collection<? extends GrantedAuthority> getAuthorities() {
+      return authorities;
+   }
 
-    @Override
-    public String getUsername() {
-        return email;
-    }
+   //
+   @Override
+   public String getPassword() {
+      return email;
+   }
 
-    /**
-     * User 클래스 상속을 받지 않아 실질적으로 필요가 없다.
-     * 게정 만료 여부,
-     * 계정 lock 여부,
-     * 인증 만료 여부,
-     * 활성 여부
-     */
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
+   @Override
+   public String getUsername() {
+      return email;
+   }
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
+   /**
+    * User 클래스 상속을 받지 않아 실질적으로 필요가 없다. 게정 만료 여부, 계정 lock 여부, 인증 만료 여부, 활성 여부
+    */
+   @Override
+   public boolean isAccountNonExpired() {
+      return true;
+   }
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
+   @Override
+   public boolean isAccountNonLocked() {
+      return true;
+   }
 
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+   @Override
+   public boolean isCredentialsNonExpired() {
+      return true;
+   }
+
+   @Override
+   public boolean isEnabled() {
+      return true;
+   }
 }
 
 
