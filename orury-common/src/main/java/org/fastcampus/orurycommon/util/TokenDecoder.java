f6aa49e677eaa158a802191e5c7291dbcf225430
YunNote <zzdd1558@gmail.com>
2024-02-12 13:40:54 +0900

    feature: ObjectMapper

--- orury-common/src/main/java/org/fastcampus/orurycommon/util/TokenDecoder.java
+++ orury-common/src/main/java/org/fastcampus/orurycommon/util/TokenDecoder.java
@@ -16,6 +16,8 @@ public class TokenDecoder {
         String payloadJWT = tokenParts[1];
         Base64.Decoder decoder = Base64.getUrlDecoder();
         String payload = new String(decoder.decode(payloadJWT));
+
+        //
         ObjectMapper objectMapper = new ObjectMapper()
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
