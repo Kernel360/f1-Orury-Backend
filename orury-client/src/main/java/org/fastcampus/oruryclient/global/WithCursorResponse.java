1d530ff20696189791689848daeee9762beea48f
Yejin Song <yjyjsssong@gmail.com>
2024-02-19 10:51:17 +0900

    refactor : Comment도 처리할 수 있도록 오버로딩

--- orury-client/src/main/java/org/fastcampus/oruryclient/global/WithCursorResponse.java
+++ orury-client/src/main/java/org/fastcampus/oruryclient/global/WithCursorResponse.java
@@ -1,7 +1,9 @@
 package org.fastcampus.oruryclient.global;
 
+import org.fastcampus.oruryclient.comment.converter.response.CommentResponse;
 import org.fastcampus.orurydomain.global.constants.NumberConstants;
 
+import java.util.Comparator;
 import java.util.List;
 
 public record WithCursorResponse<T extends IdIdentifiable>(
@@ -14,6 +16,18 @@ public record WithCursorResponse<T extends IdIdentifiable>(
                 : list.get(list.size() - 1).id();
         return new WithCursorResponse<>(list, newCursor);
     }
+
+    public static WithCursorResponse of(List<CommentResponse> list) {
+
+        Long cursor = list.stream()
+                .filter(commentResponse -> commentResponse.parentId() == NumberConstants.PARENT_COMMENT)
+                .map(CommentResponse::id)
+                .max(Comparator.naturalOrder())
+                .orElse(NumberConstants.LAST_CURSOR);
+
+        return new WithCursorResponse(list, cursor);
+    }
+
 
     private static Long determineCursorWhenEmpty(Long cursor) {
         return cursor.equals(NumberConstants.FIRST_CURSOR)
