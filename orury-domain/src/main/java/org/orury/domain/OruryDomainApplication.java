18f6db0e836c4d77948034e7a99eee987e333ed6
GBGreenBravo <gbgreenbravo@gmail.com>
2024-02-22 21:10:51 +0900

    chore : domain 모듈의 common 모듈 의존성 추가에 따른 패키지스캔 설정 추가

--- orury-domain/src/main/java/org/orury/domain/OruryDomainApplication.java
+++ orury-domain/src/main/java/org/orury/domain/OruryDomainApplication.java
@@ -3,7 +3,7 @@ package org.orury.domain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"org.orury.common"})
+@SpringBootApplication(scanBasePackages = {"org.orury.domain", "org.orury.common"})
 public class OruryDomainApplication {
     public static void main(String[] args) {
         SpringApplication.run(OruryDomainApplication.class, args);
