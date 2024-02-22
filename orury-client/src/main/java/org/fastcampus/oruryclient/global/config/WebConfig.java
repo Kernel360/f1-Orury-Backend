9cb7d52fb0bc386b7d22e113957cde017775404c
Chan <ieejo716@naver.com>
2024-02-22 12:36:12 +0900

    Refactor : SnakeCase 수정
    - 불필요한 EnableWebMvc제거
    - yml에서 적용해도 되지만 에러 발생시 코드에서 해결하기 위해  메서드로 분리

--- orury-client/src/main/java/org/fastcampus/oruryclient/global/config/WebConfig.java
+++ orury-client/src/main/java/org/fastcampus/oruryclient/global/config/WebConfig.java
@@ -1,17 +1,23 @@
 package org.fastcampus.oruryclient.global.config;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix("/api/v1", HandlerTypePredicate.forAnnotation(RestController.class));
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jacksonObjectMapperBuilderCustomizer() {
+        return builder -> builder.propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
     }
 }
