586b5039ada51474831d3ecf82bdd7edbb6562b3
GBGreenBravo <gbgreenbravo@gmail.com>
2024-02-22 20:12:15 +0900

    refactor : 비회원용 Token 만료 시, 다른 ErrorCode 반환

--- orury-common/src/main/java/org/orury/common/error/code/TokenErrorCode.java
+++ orury-common/src/main/java/org/orury/common/error/code/TokenErrorCode.java
@@ -9,7 +9,8 @@ public enum TokenErrorCode implements ErrorCode {
     EXPIRED_ACCESS_TOKEN(990, "만료된 access token 입니다."),
     EXPIRED_REFRESH_TOKEN(999, "만료된 refresh token 입니다."),
     INVALID_ACCESS_TOKEN(980, "유효하지 않은 형식의 access token 입니다."),
-    INVALID_REFRESH_TOKEN(989, "유효하지 않은 형식의 refresh token 입니다.");
+    INVALID_REFRESH_TOKEN(989, "유효하지 않은 형식의 refresh token 입니다."),
+    EXPIRED_SIGNUP_TOKEN(990, "회원가입 시간이 만료됐습니다. 다시 회원가입 해주세요.");
     private final int status;
     private final String message;
 }
