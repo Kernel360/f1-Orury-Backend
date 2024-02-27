b496cc5354f6e7442dc85538f99fbdc19d404dc0
Chan <ieejo716@naver.com>
2024-02-27 15:05:00 +0900

    Feautre : 게시판 관련 로직 추상화 + 다른 서비스 로직 구현 목적으로 작성

---
+++ orury-client/src/main/java/org/orury/client/post/application/PostFacade.java
@@ -0,0 +1,102 @@
+package org.orury.client.post.application;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.orury.client.post.common.request.PostRequest;
+import org.orury.client.post.common.response.PostResponse;
+import org.orury.client.post.common.response.PostsResponse;
+import org.orury.client.post.common.response.PostsWithPageResponse;
+import org.orury.client.post.service.PostLikeService;
+import org.orury.client.user.service.UserService;
+import org.orury.common.error.code.PostErrorCode;
+import org.orury.common.error.exception.BusinessException;
+import org.orury.domain.post.PostService;
+import org.orury.domain.post.db.PostLike;
+import org.orury.domain.post.db.PostLikePK;
+import org.orury.domain.post.dto.PostDto;
+import org.orury.domain.post.dto.PostLikeDto;
+import org.orury.domain.user.dto.UserDto;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.orury.domain.global.constants.NumberConstants.POST_PAGINATION_SIZE;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class PostFacade {
+    private final PostService postService;
+    private final UserService userService;
+    private final PostLikeService postLikeService;
+
+    public void createPost(Long userId, PostRequest request, List<MultipartFile> files) {
+        var user = userService.getUserDtoById(userId);
+        var post = request.toDto(user);
+        postService.createPost(post, files);
+    }
+
+    public void updatePost(Long userId, PostRequest request, List<MultipartFile> files) {
+        var user = userService.getUserDtoById(userId);
+        var oldPost = postService.getPostDtoById(request.id());
+        isValidate(oldPost, user);
+        var newPost = request.toDto(oldPost, user);
+        postService.createPost(newPost, files);
+    }
+
+    public void deletePost(Long userId, Long postId) {
+        var user = userService.getUserDtoById(userId);
+        var post = postService.getPostDtoById(postId);
+        isValidate(post, user);
+        postService.deletePost(post);
+    }
+
+    public PostResponse getPostById(Long userId, Long postId) {
+        PostDto post = postService.getPostDtoById(postId);
+        UserDto user = userService.getUserDtoById(userId);
+        postService.addViewCount(post);
+        boolean isLike = postLikeService.isLiked(userId, postId);
+        return PostResponse.of(post, user, isLike);
+    }
+
+    public List<PostsResponse> getPostsByCategory(int category, Long cursor, Pageable pageable) {
+        return postService.getPostDtosByCategory(category, cursor, pageable)
+                .stream()
+                .map(PostsResponse::of)
+                .toList();
+    }
+
+    public List<PostsResponse> getPostsBySearchWord(String searchWord, Long cursor) {
+        var pageRequest = PageRequest.of(0, POST_PAGINATION_SIZE);
+        return postService.getPostDtosBySearchWord(searchWord, cursor, pageRequest)
+                .stream()
+                .map(PostsResponse::of)
+                .toList();
+    }
+
+    public PostsWithPageResponse getHotPosts(int page) {
+        var pageRequest = PageRequest.of(page, POST_PAGINATION_SIZE);
+        var post = postService.getHotPostDtos(pageRequest);
+        int nextPage = postService.getNextPage(post, page);
+        return PostsWithPageResponse.of(post.stream().map(PostsResponse::of).toList(), nextPage);
+    }
+
+    public void createPostLike(Long userId, Long postId) {
+        var postLikeDto = PostLikeDto.from(PostLike.of(PostLikePK.of(userId, postId)));
+        postLikeService.createPostLike(postLikeDto);
+    }
+
+    public void deletePostLike(Long userId, Long postId) {
+        var postLikeDto = PostLikeDto.from(PostLike.of(PostLikePK.of(userId, postId)));
+        postLikeService.deletePostLike(postLikeDto);
+    }
+
+    private void isValidate(PostDto postDto, UserDto userDto) {
+        if (!Objects.equals(postDto.userDto().id(), userDto.id()))
+            throw new BusinessException(PostErrorCode.FORBIDDEN);
+    }
+}
