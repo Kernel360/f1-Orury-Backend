037c933fdd7e16a1bddab152d5904a53715b03a4
Chan <ieejo716@naver.com>
2024-02-27 15:02:42 +0900

    Feature : 게시글 읽기 관련 로직 분리

---
+++ orury-client/src/main/java/org/orury/client/post/infrastructure/PostReaderImpl.java
@@ -0,0 +1,56 @@
+package org.orury.client.post.infrastructure;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.orury.common.error.code.PostErrorCode;
+import org.orury.common.error.exception.BusinessException;
+import org.orury.domain.global.constants.NumberConstants;
+import org.orury.domain.post.PostReader;
+import org.orury.domain.post.db.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PostReaderImpl implements PostReader {
+    private final PostRepository postRepository;
+
+    @Override
+    public List<Post> findByCategoryOrderByIdDesc(int category, Long cursor, Pageable pageable) {
+        return (cursor.equals(NumberConstants.FIRST_CURSOR))
+                ? postRepository.findByCategoryOrderByIdDesc(category, pageable)
+                : postRepository.findByCategoryAndIdLessThanOrderByIdDesc(category, cursor, pageable);
+    }
+
+    @Override
+    public List<Post> findByTitleContainingOrContentContainingOrderByIdDesc(String searchWord, Long cursor, Pageable pageable) {
+        return (cursor.equals(NumberConstants.FIRST_CURSOR))
+                ? postRepository.findByTitleContainingOrContentContainingOrderByIdDesc(searchWord, searchWord, pageable)
+                : postRepository.findByIdLessThanAndTitleContainingOrIdLessThanAndContentContainingOrderByIdDesc(cursor, searchWord, cursor, searchWord, pageable);
+    }
+
+    @Override
+    public List<Post> findByUserIdOrderByIdDesc(Long userId, Long cursor, Pageable pageable) {
+        return (cursor.equals(NumberConstants.FIRST_CURSOR))
+                ? postRepository.findByUserIdOrderByIdDesc(userId, pageable)
+                : postRepository.findByUserIdAndIdLessThanOrderByIdDesc(userId, cursor, pageable);
+    }
+
+    @Override
+    public Page<Post> findByLikeCountGreaterThanEqualAndCreatedAtGreaterThanEqualOrderByLikeCountDescCreatedAtDesc(int likeCount, LocalDateTime localDateTime, Pageable pageable) {
+        return postRepository.findByLikeCountGreaterThanEqualAndCreatedAtGreaterThanEqualOrderByLikeCountDescCreatedAtDesc
+                (NumberConstants.HOT_POSTS_BOUNDARY, LocalDateTime.now()
+                        .minusMonths(1L), pageable);
+    }
+
+    @Override
+    public Post findById(Long id) {
+        return postRepository.findById(id)
+                .orElseThrow(() -> new BusinessException(PostErrorCode.NOT_FOUND));
+    }
+}
---
+++ orury-domain/src/main/java/org/orury/domain/post/PostReader.java
@@ -0,0 +1,24 @@
+package org.orury.domain.post;
+
+import org.orury.domain.post.db.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface PostReader {
+    List<Post> findByCategoryOrderByIdDesc(int category, Long cursor, Pageable pageable);
+
+    List<Post> findByTitleContainingOrContentContainingOrderByIdDesc(String searchWord, Long cursor, Pageable pageable);
+
+    List<Post> findByUserIdOrderByIdDesc(Long userId, Long cursor, Pageable pageable);
+
+    Page<Post> findByLikeCountGreaterThanEqualAndCreatedAtGreaterThanEqualOrderByLikeCountDescCreatedAtDesc(
+            int likeCount,
+            LocalDateTime localDateTime,
+            Pageable pageable
+    );
+
+    Post findById(Long id);
+}
