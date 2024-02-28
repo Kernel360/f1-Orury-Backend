f67e7fc9fd40a4e656d9b0bd282df8bf9487f15a
Yejin Song <yjyjsssong@gmail.com>
2024-02-28 21:26:04 +0900

    Refactor : 리팩토링된 도메인 repo-> reader, store 변경 및 BusinessException 처리

--- orury-domain/src/main/java/org/orury/domain/user/domain/UserServiceImpl.java
+++ orury-domain/src/main/java/org/orury/domain/user/domain/UserServiceImpl.java
@@ -2,14 +2,16 @@ package org.orury.domain.user.domain;
 
 import org.orury.common.error.exception.BusinessException;
 import org.orury.common.util.ImageUrlConverter;
+import org.orury.domain.global.image.ImageReader;
+import org.orury.domain.global.image.ImageStore;
 import org.orury.common.util.S3Folder;
 import org.orury.domain.comment.domain.CommentStore;
-import org.orury.domain.global.domain.ImageUtils;
 import org.orury.domain.gym.domain.GymStore;
 import org.orury.domain.post.domain.PostStore;
 import org.orury.domain.review.domain.ReviewStore;
 import org.orury.domain.user.domain.dto.UserDto;
 import org.orury.domain.user.domain.entity.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,16 +25,22 @@ import lombok.extern.slf4j.Slf4j;
 public class UserServiceImpl implements UserService {
     private final UserReader userReader;
     private final UserStore userStore;
-    private final ImageUtils imageUtils;
+    private final ImageReader imageReader;
+    private final ImageStore imageStore;
     private final PostStore postStore;
     private final CommentStore commentStore;
     private final ReviewStore reviewStore;
     private final GymStore gymStore;
 
+    @Value("${cloud.aws.s3.default-image}")
+    private String defaultImage;
+
     @Override
+    @Transactional(readOnly = true)
     public UserDto getUserDtoById(Long id) {
-        User user = userReader.findUserById(id);
-        String profileUrl = imageUtils.getUserImageUrl(user.getProfileImage());
+        User user = userReader.findUserById(id)
+                .orElseThrow(() -> new BusinessException(UserErrorCode.NOT_FOUND));
+        String profileUrl = imageReader.getUserImageLink(user.getProfileImage());
         return UserDto.from(user, profileUrl);
     }
 
@@ -40,8 +48,7 @@ public class UserServiceImpl implements UserService {
     @Transactional
     public void updateProfileImage(UserDto userDto, MultipartFile image) throws BusinessException {
         //기존에 저장된 요소 삭제
-        imageUtils.oldS3ImagesDelete(S3Folder.USER.getName(), userDto.profileImage());
-
+        imageStore.delete(userDto.profileImage());
         imageUploadAndSave(userDto, image);
     }
 
@@ -59,21 +66,14 @@ public class UserServiceImpl implements UserService {
         postStore.deletePostLikesByUserId(userDto.id());
         postStore.deletePostsByUserId(userDto.id());
         reviewStore.deleteReviewReactionsByUserId(userDto.id());
-
-        imageUtils.oldS3ImagesDelete(S3Folder.USER.getName(), userDto.profileImage());
-        var deletingUser = userDto.toEntity().delete(imageUtils.getUserDefaultImage());
-
+        imageStore.delete(userDto.profileImage());
+        var deletingUser = userDto.toEntity().delete(defaultImage);
         userStore.save(deletingUser);
     }
 
-    private void imageUploadAndSave(UserDto userDto, MultipartFile file) {
-        //MultipartFile이 오지 않은 경우 -> 유저의 프로필 이미지를 기본 이미지로 변경
-        if (file == null || file.isEmpty()) {
-            userStore.save(userDto.toEntity(imageUtils.getUserDefaultImage()));
-        } else {
-            //MultipartFile이 오는 경우 -> 유저의 프로필 이미지를 업로드한 이미지로 변경
-            String image = imageUtils.upload(S3Folder.USER.getName(), file);
-            userStore.save(userDto.toEntity(image));
-        }
+    void imageUploadAndSave(UserDto userDto, MultipartFile file) {
+        // 빈 파일이 들어왔는지에 대해서는 upload 메소드에서 유효성 검사해줌.
+        String image = imageStore.upload(file);
+        userStore.save(userDto.toEntity(image));
     }
 }
