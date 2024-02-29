baf494274f8125af362ccb782a36b68d5233d6e8
Yejin Song <yjyjsssong@gmail.com>
2024-02-29 15:55:03 +0900

    Refactor : DDD 리팩토링 완료된 Review, Comment 반영

--- orury-client/src/main/java/org/orury/client/user/application/UserFacade.java
+++ orury-client/src/main/java/org/orury/client/user/application/UserFacade.java
@@ -1,14 +1,18 @@
 package org.orury.client.user.application;
 
 
-import lombok.RequiredArgsConstructor;
+import org.orury.client.comment.application.CommentService;
 import org.orury.client.global.WithCursorResponse;
+import org.orury.client.review.application.ReviewService;
 import org.orury.client.user.interfaces.request.UserInfoRequest;
+import org.orury.client.user.interfaces.response.MyCommentResponse;
 import org.orury.client.user.interfaces.response.MyPostResponse;
+import org.orury.client.user.interfaces.response.MyReviewResponse;
+import org.orury.domain.comment.domain.dto.CommentDto;
 import org.orury.domain.global.constants.NumberConstants;
 import org.orury.domain.post.domain.PostService;
 import org.orury.domain.post.domain.dto.PostDto;
-import org.orury.domain.user.domain.UserService;
+import org.orury.domain.review.domain.dto.ReviewDto;
 import org.orury.domain.user.domain.dto.UserDto;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
@@ -16,11 +20,15 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+import lombok.RequiredArgsConstructor;
+
 @Component
 @RequiredArgsConstructor
 public class UserFacade {
     private final UserService userService;
     private final PostService postService;
+    private final ReviewService reviewService;
+    private final CommentService commentService;
 
     public UserDto readMypage(Long id) {
         return userService.getUserDtoById(id);
@@ -45,6 +53,29 @@ public class UserFacade {
                 .toList();
 
         WithCursorResponse<MyPostResponse> cursorResponse = WithCursorResponse.of(myPostResponses, cursor);
+
+        return cursorResponse;
+    }
+
+    public WithCursorResponse<MyReviewResponse> getReviewsByUserId(Long id, Long cursor) {
+        List<ReviewDto> reviewDtos = reviewService.getReviewDtosByUserId(id, cursor, PageRequest.of(0, NumberConstants.POST_PAGINATION_SIZE));
+        List<MyReviewResponse> myReviewResponses = reviewDtos.stream()
+                .map(MyReviewResponse::of)
+                .toList();
+
+        WithCursorResponse<MyReviewResponse> cursorResponse = WithCursorResponse.of(myReviewResponses, cursor);
+
+        return cursorResponse;
+    }
+
+    public WithCursorResponse<MyCommentResponse> getCommentsByUserId(Long id, Long cursor) {
+        List<CommentDto> commmentDtos = commentService.getCommentDtosByUserId(id, cursor);
+
+        List<MyCommentResponse> myCommentResponses = commmentDtos.stream()
+                .map(MyCommentResponse::of)
+                .toList();
+
+        WithCursorResponse<MyCommentResponse> cursorResponse = WithCursorResponse.of(myCommentResponses, cursor);
 
         return cursorResponse;
     }
