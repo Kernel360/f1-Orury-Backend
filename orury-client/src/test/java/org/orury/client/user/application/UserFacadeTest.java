d043cb74f56c58e5566bfb1f8b9f970f5f1bbde0
Yejin Song <yjyjsssong@gmail.com>
2024-02-29 15:55:59 +0900

    Feat : Facade 리팩토링에 따른 TestCode 추가 작성

--- orury-client/src/test/java/org/orury/client/user/application/UserFacadeTest.java
+++ orury-client/src/test/java/org/orury/client/user/application/UserFacadeTest.java
@@ -11,15 +11,19 @@ import static org.mockito.Mockito.times;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.orury.client.comment.service.CommentService;
+import org.orury.client.comment.application.CommentService;
 import org.orury.client.global.WithCursorResponse;
-import org.orury.client.post.service.PostService;
-import org.orury.client.review.service.ReviewService;
+import org.orury.client.review.application.ReviewService;
 import org.orury.client.user.interfaces.request.UserInfoRequest;
+import org.orury.client.user.interfaces.response.MyCommentResponse;
 import org.orury.client.user.interfaces.response.MyPostResponse;
+import org.orury.client.user.interfaces.response.MyReviewResponse;
+import org.orury.domain.comment.domain.dto.CommentDto;
 import org.orury.domain.global.constants.NumberConstants;
+import org.orury.domain.gym.domain.dto.GymDto;
+import org.orury.domain.post.domain.PostService;
 import org.orury.domain.post.domain.dto.PostDto;
-import org.orury.domain.user.domain.UserService;
+import org.orury.domain.review.domain.dto.ReviewDto;
 import org.orury.domain.user.domain.dto.UserDto;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -97,8 +101,8 @@ class UserFacadeTest {
     }
 
     @Test
-    @DisplayName("getPostsByUserId(Long id, Long cursor) test: id, cursor 값을 입력받아 WithCursorResponse을 반환한다. [성공] ")
-    void should_returnWithCursorResponse() {
+    @DisplayName("getPostsByUserId(Long id, Long cursor) test: id, cursor 값을 입력받아 내가 쓴 게시글을 반환한다. [성공] ")
+    void should_returnWithCursorPostsResponse() {
         //given
         Long userId = 1L;
         Long cursor = 0L;
@@ -120,6 +124,58 @@ class UserFacadeTest {
         //then
         assertThat(actualResponse).isEqualTo(response);
         then(postService).should(times(1)).getPostDtosByUserId(anyLong(), anyLong(), any());
+    }
+
+    @Test
+    @DisplayName("getReviewsByUserId(Long id, Long cursor) test: id, cursor 값을 입력받아 내가 쓴 리뷰를 반환한다. [성공] ")
+    void should_returnWithCursorReviewsResponse() {
+        //given
+        Long userId = 1L;
+        Long cursor = 0L;
+
+        List<ReviewDto> reviewDtos = new ArrayList<>();
+        for (int i = 10; i >= 1; i--) {
+            reviewDtos.add(createReviewDto((long) i));
+        }
+
+        WithCursorResponse<MyReviewResponse> response = WithCursorResponse.of(reviewDtos.stream()
+                .map(MyReviewResponse::of)
+                .toList(), cursor);
+
+        given(reviewService.getReviewDtosByUserId(anyLong(), anyLong(), any())).willReturn(reviewDtos);
+
+        //when
+        WithCursorResponse<MyReviewResponse> actualResponse = userFacade.getReviewsByUserId(userId, cursor);
+
+        //then
+        assertThat(actualResponse).isEqualTo(response);
+        then(reviewService).should(times(1)).getReviewDtosByUserId(anyLong(), anyLong(), any());
+    }
+
+    @Test
+    @DisplayName("getCommentsByUserId(Long id, Long cursor) test: id, cursor 값을 입력받아 내가 쓴 댓글을 반환한다. [성공] ")
+    void should_returnWithCommentsCursorResponse() {
+        //given
+        Long userId = 1L;
+        Long cursor = 0L;
+
+        List<CommentDto> commentDtos = new ArrayList<>();
+        for (int i = 10; i >= 1; i--) {
+            commentDtos.add(createCommentDto((long) i));
+        }
+
+        WithCursorResponse<MyCommentResponse> response = WithCursorResponse.of(commentDtos.stream()
+                .map(MyCommentResponse::of)
+                .toList(), cursor);
+
+        given(commentService.getCommentDtosByUserId(anyLong(), anyLong())).willReturn(commentDtos);
+
+        //when
+        WithCursorResponse<MyCommentResponse> actualResponse = userFacade.getCommentsByUserId(userId, cursor);
+
+        //then
+        assertThat(actualResponse).isEqualTo(response);
+        then(commentService).should(times(1)).getCommentDtosByUserId(anyLong(), anyLong());
     }
 
     @Test
@@ -169,6 +225,69 @@ class UserFacadeTest {
                 createUserDto(id),
                 LocalDateTime.now(),
                 LocalDateTime.now()
+        );
+    }
+
+    private GymDto createGymDto() {
+        return GymDto.of(
+                1L,
+                "더클라임 봉은사점",
+                "kakaoid",
+                "서울시 도로명주소",
+                "서울시 지번주소",
+                25.3f,
+                23,
+                12,
+                List.of(),
+                "37.513709",
+                "127.062144",
+                "더클라임",
+                "01012345678",
+                "instalink.com",
+                "MONDAY",
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                "11:00-23:11",
+                "12:00-23:22",
+                "13:00-23:33",
+                "14:00-23:44",
+                "15:00-23:55",
+                "16:00-23:66",
+                "17:00-23:77",
+                "gymHomepageLink",
+                "gymRemark"
+        );
+    }
+
+    private ReviewDto createReviewDto(Long id) {
+        return ReviewDto.of(
+                id,
+                "test content",
+                List.of(),
+                4.5f,
+                1,
+                2,
+                3,
+                4,
+                5,
+                createUserDto(1L),
+                createGymDto(),
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+    }
+
+    private CommentDto createCommentDto(Long id) {
+        return CommentDto.of(
+                id,
+                "content",
+                1L,
+                0,
+                createPostDto(id),
+                createUserDto(1L),
+                0,
+                null,
+                null
         );
     }
 
