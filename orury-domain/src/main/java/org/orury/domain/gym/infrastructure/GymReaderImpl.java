4307052f96770023d8acb0f131b803896f9da721
GBGreenBravo <gbgreenbravo@gmail.com>
2024-03-02 12:20:11 +0900

    feat : 좌표내 암장 검색 SpringDataJpa 메서드 구현

--- orury-domain/src/main/java/org/orury/domain/gym/infrastructure/GymReaderImpl.java
+++ orury-domain/src/main/java/org/orury/domain/gym/infrastructure/GymReaderImpl.java
@@ -7,6 +7,7 @@ import org.orury.domain.gym.domain.entity.GymLikePK;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
@@ -28,6 +29,15 @@ public class GymReaderImpl implements GymReader {
     @Override
     public List<Gym> findGymsBySearchWord(String searchWord) {
         return gymRepository.findByNameContaining(searchWord);
+    }
+
+    public List<Gym> findGymsInAreaGrid(Map<String, Double> gridMap) {
+        return gymRepository.findByLatitudeBetweenAndLongitudeBetweenOrderByLikeCount(
+                gridMap.get("bottom"),
+                gridMap.get("top"),
+                gridMap.get("left"),
+                gridMap.get("right")
+        );
     }
 
     @Override
