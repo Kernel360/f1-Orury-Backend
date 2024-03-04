1c9c53821f6d09c3e175157dbdfad0859a473f7d
GBGreenBravo <gbgreenbravo@gmail.com>
2024-03-04 10:35:22 +0900

    refactor : (User) Reader/Store 메서드 추가

--- orury-domain/src/main/java/org/orury/domain/user/domain/UserReader.java
+++ orury-domain/src/main/java/org/orury/domain/user/domain/UserReader.java
@@ -2,6 +2,10 @@ package org.orury.domain.user.domain;
 
 import org.orury.domain.user.domain.entity.User;
 
+import java.util.Optional;
+
 public interface UserReader {
     User findUserById(Long id);
+
+    Optional<User> findByEmail(String email);
 }
--- orury-domain/src/main/java/org/orury/domain/user/infrastucture/UserStoreImpl.java
+++ orury-domain/src/main/java/org/orury/domain/user/infrastucture/UserStoreImpl.java
@@ -1,7 +1,10 @@
 package org.orury.domain.user.infrastucture;
 
+import org.orury.common.error.code.UserErrorCode;
+import org.orury.common.error.exception.BusinessException;
 import org.orury.domain.user.domain.UserStore;
 import org.orury.domain.user.domain.entity.User;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Repository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,6 +17,11 @@ public class UserStoreImpl implements UserStore {
     @Override
     public void save(User entity) {
         userRepository.save(entity);
+    }
+
+    @Override
+    public void saveAndFlush(User user) {
+        userRepository.saveAndFlush(user);
     }
 
 
