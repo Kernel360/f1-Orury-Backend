725b964913fbd903954cd8b8d147a6f50c642959
Chan <ieejo716@naver.com>
2024-03-04 11:41:40 +0900

    Feature : 공지사항 Facade

---
+++ orury-admin/src/main/java/org/orury/admin/notice/application/NoticeFacade.java
@@ -0,0 +1,45 @@
+package org.orury.admin.notice.application;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.orury.admin.notice.interfaces.request.NoticeRequest;
+import org.orury.domain.admin.domain.AdminServiceImpl;
+import org.orury.domain.notice.domain.NoticeService;
+import org.orury.domain.notice.domain.dto.NoticeDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class NoticeFacade {
+    private final NoticeService noticeService;
+    private final AdminServiceImpl adminServiceImpl;
+
+    public void createNotice(Long adminId, NoticeRequest request) {
+        var admin = adminServiceImpl.getAdmin(adminId);
+        var notice = request.toDto(admin);
+        noticeService.createNotice(admin, notice);
+    }
+
+    public void updateNotice(Long adminId, NoticeRequest request) {
+        var admin = adminServiceImpl.getAdmin(adminId);
+        var oldNotice = noticeService.getNotice(request.id());
+        var newNotice = request.toDto(admin, oldNotice);
+        noticeService.createNotice(admin, newNotice);
+    }
+
+    public void deleteNotice(Long adminId, Long noticeId) {
+        adminServiceImpl.getAdmin(adminId);
+        noticeService.deleteNotice(noticeId);
+    }
+
+    public NoticeDto getNotice(Long noticeId) {
+        return noticeService.getNotice(noticeId);
+    }
+
+    public List<NoticeDto> getNotices() {
+        return noticeService.getNotices();
+    }
+}
