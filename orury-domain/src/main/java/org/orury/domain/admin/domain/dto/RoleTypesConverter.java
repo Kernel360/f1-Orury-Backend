4048c923769465a66079e0a8b4fa6b7cd0c32a08
Chan <ieejo716@naver.com>
2024-03-04 11:40:19 +0900

    Refactor : Role_Type Converter

---
+++ orury-domain/src/main/java/org/orury/domain/admin/domain/dto/RoleTypesConverter.java
@@ -0,0 +1,29 @@
+package org.orury.domain.admin.domain.dto;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Converter
+public class RoleTypesConverter implements AttributeConverter<Set<RoleType>, String> {
+
+    private static final String DELIMITER = ",";
+
+    @Override
+    public String convertToDatabaseColumn(Set<RoleType> attribute) {
+        return attribute.stream()
+                .map(RoleType::name)
+                .sorted()
+                .collect(Collectors.joining(DELIMITER));
+    }
+
+    @Override
+    public Set<RoleType> convertToEntityAttribute(String dbData) {
+        return Arrays.stream(dbData.split(DELIMITER))
+                .map(RoleType::valueOf)
+                .collect(Collectors.toSet());
+    }
+}
