26914d753e8502ebf6d1f721280e8da665751ed0
Yejin Song <yjyjsssong@gmail.com>
2024-03-05 19:37:01 +0900

    Feat : CrewReader 생성

--- orury-domain/src/main/java/org/orury/domain/crew/infrastructures/CrewReaderImpl.java
+++ orury-domain/src/main/java/org/orury/domain/crew/infrastructures/CrewReaderImpl.java
@@ -1,11 +1,51 @@
 package org.orury.domain.crew.infrastructures;
 
-import lombok.RequiredArgsConstructor;
 import org.orury.domain.crew.domain.CrewReader;
+import org.orury.domain.crew.domain.entity.Crew;
+import org.orury.domain.crew.domain.entity.CrewMember;
+import org.orury.domain.crew.domain.entity.CrewMemberPK;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
 public class CrewReaderImpl implements CrewReader {
     private final CrewRepository crewRepository;
+    private final CrewMemberRepository crewMemberRepository;
+
+    @Override
+    public Optional<Crew> findCrewById(Long crewId) {
+        return crewRepository.findById(crewId);
+    }
+
+    @Override
+    public Page<Crew> getCrewsByRank(Pageable pageable) {
+        return crewRepository.findByOrderByMemberCountDesc(pageable);
+    }
+
+    @Override
+    public Page<Crew> getCrewsByRecommend(Pageable pageable) {
+        return crewRepository.findByOrderByCreatedAtDesc(pageable);
+    }
+
+    @Override
+    public Page<Crew> getCrewsByCrewId(List<Long> crewIds, Pageable pageable) {
+        return crewRepository.findAllByIdIn(crewIds);
+    }
+
+    @Override
+    public List<CrewMember> getCrewMembersByUserId(Long userId) {
+        return null;
+    }
+
+    @Override
+    public boolean existCrewMember(CrewMemberPK crewMemberPK) {
+        return crewMemberRepository.existsById(crewMemberPK);
+    }
 }
