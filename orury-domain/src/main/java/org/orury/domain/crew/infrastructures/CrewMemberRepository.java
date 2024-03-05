0fa4b944442acdd87c2d1112586e6ba96b3cc5cf
GBGreenBravo <gbgreenbravo@gmail.com>
2024-03-05 15:49:47 +0900

    feat : Crew 관련 Infrastructures layer 구현

--- orury-domain/src/main/java/org/orury/domain/crew/infrastructures/CrewMemberRepository.java
+++ orury-domain/src/main/java/org/orury/domain/crew/infrastructures/CrewMemberRepository.java
@@ -5,4 +5,5 @@ import org.orury.domain.crew.domain.entity.CrewMemberPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CrewMemberRepository extends JpaRepository<CrewMember, CrewMemberPK> {
+    boolean existsByCrewMemberPK_CrewIdAndCrewMemberPK_UserId(Long crewId, Long userId);
 }
--- orury-domain/src/main/java/org/orury/domain/crew/infrastructures/CrewReaderImpl.java
+++ orury-domain/src/main/java/org/orury/domain/crew/infrastructures/CrewReaderImpl.java
@@ -2,10 +2,18 @@ package org.orury.domain.crew.infrastructures;
 
 import lombok.RequiredArgsConstructor;
 import org.orury.domain.crew.domain.CrewReader;
+import org.orury.domain.crew.domain.entity.Crew;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class CrewReaderImpl implements CrewReader {
     private final CrewRepository crewRepository;
+
+    @Override
+    public Optional<Crew> findById(Long crewId) {
+        return crewRepository.findById(crewId);
+    }
 }
