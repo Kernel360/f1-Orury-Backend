51157af77fe29ac45eedae85ebe3cdae7b362443
GBGreenBravo <gbgreenbravo@gmail.com>
2024-03-05 15:49:35 +0900

    feat : Meeting 관련 Infrastructures layer 구현

--- orury-domain/src/main/java/org/orury/domain/meeting/infrastructure/MeetingMemberRepository.java
+++ orury-domain/src/main/java/org/orury/domain/meeting/infrastructure/MeetingMemberRepository.java
@@ -2,7 +2,16 @@ package org.orury.domain.meeting.infrastructure;
 
 import org.orury.domain.meeting.domain.entity.MeetingMember;
 import org.orury.domain.meeting.domain.entity.MeetingMemberPK;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MeetingMemberRepository extends JpaRepository<MeetingMember, MeetingMemberPK> {
+    boolean existsByMeetingMemberPK_MeetingIdAndMeetingMemberPK_UserId(Long meetingId, Long userId);
+
+    Page<MeetingMember> findByMeetingMemberPK_MeetingId(Long meetingId, PageRequest pageRequest);
+
+    List<MeetingMember> findByMeetingMemberPK_MeetingId(Long meetingId);
 }
--- orury-domain/src/main/java/org/orury/domain/meeting/infrastructure/MeetingReaderImpl.java
+++ orury-domain/src/main/java/org/orury/domain/meeting/infrastructure/MeetingReaderImpl.java
@@ -2,10 +2,30 @@ package org.orury.domain.meeting.infrastructure;
 
 import lombok.RequiredArgsConstructor;
 import org.orury.domain.meeting.domain.MeetingReader;
+import org.orury.domain.meeting.domain.entity.Meeting;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class MeetingReaderImpl implements MeetingReader {
     private final MeetingRepository meetingRepository;
+
+    @Override
+    public Optional<Meeting> findById(Long meetingId) {
+        return meetingRepository.findById(meetingId);
+    }
+
+    @Override
+    public List<Meeting> getNotEndedMeetingsByCrewId(Long crewId) {
+        return meetingRepository.findByCrew_IdAndEndTimeBeforeOrderByIdDesc(crewId, LocalDateTime.now());
+    }
+
+    @Override
+    public List<Meeting> getEndedMeetingsByCrewId(Long crewId) {
+        return meetingRepository.findByCrew_IdAndEndTimeAfterOrderByIdDesc(crewId, LocalDateTime.now());
+    }
 }
