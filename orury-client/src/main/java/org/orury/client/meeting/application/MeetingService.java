8d6e245f2091c62055dc9a58b6374080fd5f864e
GBGreenBravo <gbgreenbravo@gmail.com>
2024-03-05 15:47:33 +0900

    feat : Meeting 관련 Application layer 구현

---
+++ orury-client/src/main/java/org/orury/client/meeting/application/MeetingService.java
@@ -0,0 +1,28 @@
+package org.orury.client.meeting.application;
+
+import org.orury.domain.meeting.domain.dto.MeetingDto;
+import org.orury.domain.user.domain.dto.UserDto;
+
+import java.util.List;
+
+public interface MeetingService {
+    MeetingDto getMeetingDtoById(Long meetingId);
+
+    void createMeeting(MeetingDto meetingDto);
+
+    List<MeetingDto> getPresentMeetingDtosByCrewId(Long crewId, Long userId);
+
+    List<MeetingDto> getPastMeetingDtosByCrewId(Long crewId, Long userId);
+
+    List<String> getUserImagesByMeetingId(Long memberId);
+
+    void updateMeeting(MeetingDto meetingDto, Long userId);
+
+    void deleteMeeting(MeetingDto meetingDto, Long userId);
+
+    void addMeetingMember(MeetingDto meetingDto, Long userId);
+
+    void removeMeetingMember(MeetingDto meetingDto, Long userId);
+
+    List<UserDto> getMeetingMembersByMeeting(MeetingDto meetingDto);
+}
---
+++ orury-client/src/main/java/org/orury/client/meeting/application/MeetingServiceImpl.java
@@ -0,0 +1,161 @@
+package org.orury.client.meeting.application;
+
+import lombok.RequiredArgsConstructor;
+import org.orury.common.error.code.CrewErrorCode;
+import org.orury.common.error.code.MeetingErrorCode;
+import org.orury.common.error.exception.BusinessException;
+import org.orury.domain.crew.domain.CrewMemberReader;
+import org.orury.domain.global.constants.NumberConstants;
+import org.orury.domain.global.image.ImageReader;
+import org.orury.domain.meeting.domain.MeetingMemberReader;
+import org.orury.domain.meeting.domain.MeetingMemberStore;
+import org.orury.domain.meeting.domain.MeetingReader;
+import org.orury.domain.meeting.domain.MeetingStore;
+import org.orury.domain.meeting.domain.dto.MeetingDto;
+import org.orury.domain.meeting.domain.dto.MeetingMemberDto;
+import org.orury.domain.meeting.domain.entity.Meeting;
+import org.orury.domain.meeting.domain.entity.MeetingMember;
+import org.orury.domain.meeting.domain.entity.MeetingMemberPK;
+import org.orury.domain.user.domain.UserReader;
+import org.orury.domain.user.domain.dto.UserDto;
+import org.orury.domain.user.domain.entity.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+import static org.orury.common.util.S3Folder.GYM;
+
+@Service
+@RequiredArgsConstructor
+public class MeetingServiceImpl implements MeetingService {
+    private final MeetingReader meetingReader;
+    private final MeetingStore meetingStore;
+    private final MeetingMemberReader meetingMemberReader;
+    private final MeetingMemberStore meetingMemberStore;
+    private final CrewMemberReader crewMemberReader;
+    private final UserReader userReader;
+    private final ImageReader imageReader;
+
+    @Override
+    @Transactional(readOnly = true)
+    public MeetingDto getMeetingDtoById(Long meetingId) {
+        Meeting meeting = meetingReader.findById(meetingId)
+                .orElseThrow(() -> new BusinessException(MeetingErrorCode.NOT_FOUND));
+        return MeetingDto.from(meeting);
+    }
+
+    @Override
+    @Transactional
+    public void createMeeting(MeetingDto meetingDto) {
+        validateCrewMember(meetingDto.crewDto().id(), meetingDto.userDto().id());
+        validateTimes(meetingDto.startTime(), meetingDto.endTime());
+        meetingStore.createMeeting(meetingDto.toEntity());
+        MeetingMemberDto meetingMemberDto = MeetingMemberDto.of(MeetingMemberPK.of(meetingDto.userDto().id(), meetingDto.id()));
+        meetingMemberStore.addMember(meetingMemberDto);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<MeetingDto> getPresentMeetingDtosByCrewId(Long crewId, Long userId) {
+        validateCrewMember(crewId, userId);
+        List<Meeting> meetings = meetingReader.getNotEndedMeetingsByCrewId(crewId);
+        return convertMeetingsToMeetingDtos(meetings, userId);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<MeetingDto> getPastMeetingDtosByCrewId(Long crewId, Long userId) {
+        validateCrewMember(crewId, userId);
+        List<Meeting> meetings = meetingReader.getEndedMeetingsByCrewId(crewId);
+        return convertMeetingsToMeetingDtos(meetings, userId);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<String> getUserImagesByMeetingId(Long memberId) {
+        List<MeetingMember> meetingMembers = meetingMemberReader.getMeetingMembersByMeetingIdMaximum(memberId, NumberConstants.MAXIMUM_OF_MEETING_THUMBNAILS);
+        return meetingMembers.stream().map(meetingMember -> {
+            User user = userReader.getUserById(meetingMember.getMeetingMemberPK().getUserId());
+            return imageReader.getUserImageLink(user.getProfileImage());
+        }).toList();
+    }
+
+    @Override
+    @Transactional
+    public void updateMeeting(MeetingDto meetingDto, Long userId) {
+        validateMeetingCreator(meetingDto.userDto().id(), userId);
+        validateTimes(meetingDto.startTime(), meetingDto.endTime());
+        if (meetingDto.capacity() < meetingDto.memberCount())
+            throw new BusinessException(MeetingErrorCode.CAPACITY_FORBIDDEN);
+        meetingStore.updateMeeting(meetingDto.toEntity());
+    }
+
+    @Override
+    @Transactional
+    public void deleteMeeting(MeetingDto meetingDto, Long userId) {
+        validateMeetingCreator(meetingDto.userDto().id(), userId);
+        meetingStore.deleteMeeting(meetingDto.id());
+    }
+
+    @Override
+    @Transactional
+    public void addMeetingMember(MeetingDto meetingDto, Long userId) {
+        validateCrewMember(meetingDto.crewDto().id(), userId);
+        if (meetingMemberReader.existsByMeetingIdAndUserId(meetingDto.id(), userId))
+            throw new BusinessException(MeetingErrorCode.ALREADY_JOINED_MEETING);
+        MeetingMemberDto meetingMemberDto = MeetingMemberDto.of(MeetingMemberPK.of(userId, meetingDto.id()));
+        meetingMemberStore.addMember(meetingMemberDto);
+    }
+
+    @Override
+    @Transactional
+    public void removeMeetingMember(MeetingDto meetingDto, Long userId) {
+        validateCrewMember(meetingDto.crewDto().id(), userId);
+        if (Objects.equals(meetingDto.userDto().id(), userId))
+            throw new BusinessException(MeetingErrorCode.MEETING_CREATOR);
+        if (!meetingMemberReader.existsByMeetingIdAndUserId(meetingDto.id(), userId))
+            throw new BusinessException(MeetingErrorCode.NOT_JOINED_MEETING);
+        MeetingMemberDto meetingMemberDto = MeetingMemberDto.of(MeetingMemberPK.of(userId, meetingDto.id()));
+        meetingMemberStore.removeMember(meetingMemberDto);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<UserDto> getMeetingMembersByMeeting(MeetingDto meetingDto) {
+        List<MeetingMember> meetingMembers = meetingMemberReader.getMeetingMembersByMeetingId(meetingDto.id());
+        return meetingMembers.stream().map(meetingMember -> {
+            User user = userReader.getUserById(meetingMember.getMeetingMemberPK().getUserId());
+            String userImage = imageReader.getUserImageLink(user.getProfileImage());
+            return UserDto.from(user, userImage);
+        }).toList();
+    }
+
+    private void validateMeetingCreator(Long meetingCreatorId, Long userId) {
+        if (!Objects.equals(meetingCreatorId, userId))
+            throw new BusinessException(MeetingErrorCode.FORBIDDEN);
+    }
+
+    private void validateCrewMember(Long crewId, Long userId) {
+        if (!crewMemberReader.existByCrewIdAndUserId(crewId, userId))
+            throw new BusinessException(CrewErrorCode.NOT_CREW_MEMBER);
+    }
+
+    private void validateTimes(LocalDateTime startTime, LocalDateTime endTime) {
+        if (startTime.isAfter(endTime))
+            throw new BusinessException(MeetingErrorCode.TURNED_OVER_TIMES);
+        if (!Objects.equals(startTime.getDayOfYear(), endTime.getDayOfYear()))
+            throw new BusinessException(MeetingErrorCode.NOT_SAME_DAY);
+    }
+
+    private List<MeetingDto> convertMeetingsToMeetingDtos(List<Meeting> meetings, Long userId) {
+        return meetings.stream().map(meeting -> {
+            boolean isParticipated = meetingMemberReader.existsByMeetingIdAndUserId(meeting.getId(), userId);
+            String userImage = imageReader.getUserImageLink(meeting.getUser().getProfileImage());
+            List<String> gymImages = imageReader.getImageLinks(GYM, meeting.getGym().getImages());
+            return MeetingDto.from(meeting, isParticipated, userImage, gymImages);
+        }).toList();
+    }
+}
