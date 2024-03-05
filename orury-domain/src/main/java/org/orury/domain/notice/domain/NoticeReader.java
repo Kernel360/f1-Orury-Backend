56ef6f7c5da77ba3de08d7212737e4e89983c391
Chan <ieejo716@naver.com>
2024-03-05 16:27:00 +0900

    Refactor : Optional 객체 반환으로 변경

--- orury-domain/src/main/java/org/orury/domain/notice/domain/NoticeReader.java
+++ orury-domain/src/main/java/org/orury/domain/notice/domain/NoticeReader.java
@@ -3,9 +3,10 @@ package org.orury.domain.notice.domain;
 import org.orury.domain.notice.domain.entity.Notice;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NoticeReader {
-    Notice getNotice(Long noticeId);
+    Optional<Notice> getNotice(Long noticeId);
 
     List<Notice> getNotices();
 }
