7ecc4fc11bb01784e125207b247c2279d2f33960
Chan <ieejo716@naver.com>
2024-03-05 17:34:17 +0900

    Refactor : UserStatus 재정의

--- orury-domain/src/main/java/org/orury/domain/user/domain/dto/UserStatus.java
+++ orury-domain/src/main/java/org/orury/domain/user/domain/dto/UserStatus.java
@@ -4,15 +4,18 @@ import lombok.Getter;
 
 @Getter
 public enum UserStatus {
-    E("ROLE_USER", "정상 유저"),
-    B("ROLE_BAN", "정책 위반 으로 제제된 유저"),
-    L("ROLE_LEAVE", "탈퇴한 유저");
+    ENABLE("ROLE_ENABLE", "정상 유저", "E"),
+    DISABLE("ROLE_DISABLE", "오래동안 접속 하지 않은 유저", "D"),
+    BAN("ROLE_BAN", "정책 위반 으로 제제된 유저", "B"),
+    LEAVE("ROLE_LEAVE", "탈퇴한 유저", "L");
 
     private final String status;
     private final String description;
+    private final String code;
 
-    UserStatus(String status, String description) {
+    UserStatus(String status, String description, String code) {
         this.status = status;
         this.description = description;
+        this.code = code;
     }
 }
