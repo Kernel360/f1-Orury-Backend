e072a43d5bdc4c83ad87c9b0a630011add25ec59
Yejin Song <yjyjsssong@gmail.com>
2024-03-05 19:38:14 +0900

    Feat : 필요한 CrewRepository 메서드 추가

--- orury-domain/src/main/java/org/orury/domain/crew/infrastructures/CrewRepository.java
+++ orury-domain/src/main/java/org/orury/domain/crew/infrastructures/CrewRepository.java
@@ -1,7 +1,23 @@
 package org.orury.domain.crew.infrastructures;
 
 import org.orury.domain.crew.domain.entity.Crew;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CrewRepository extends JpaRepository<Crew, Long> {
+
+    Page<Crew> findByOrderByMemberCountDesc(Pageable pageable);
+
+    Page<Crew> findByOrderByCreatedAtDesc(Pageable pageable);
+
+    Page<Crew> findAllByIdIn(List<Long> crewIds);
 }
+
+
+
+
+
+
