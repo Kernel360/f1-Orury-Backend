1c4d0a919fc1219a5d8e6817b5f967d31329cfe9
GBGreenBravo <gbgreenbravo@gmail.com>
2024-03-06 11:22:41 +0900

    chore : 어휘 변경 (모임 -> 일정)

--- orury-client/src/main/java/org/orury/client/meeting/interfaces/MeetingController.java
+++ orury-client/src/main/java/org/orury/client/meeting/interfaces/MeetingController.java
@@ -24,56 +24,56 @@ import static org.orury.client.meeting.interfaces.message.MeetingMessage.*;
 public class MeetingController {
     private final MeetingFacade meetingFacade;
 
-    @Operation(summary = "모임 생성", description = "MeetingCreateRequest를 받아, 모임을 생성한다.")
+    @Operation(summary = "일정 생성", description = "MeetingCreateRequest를 받아, 일정을 생성한다.")
     @PostMapping("/meetings")
     public ApiResponse createMeeting(@RequestBody MeetingCreateRequest request, @AuthenticationPrincipal UserPrincipal userPrincipal) {
         meetingFacade.createMeeting(request, userPrincipal.id());
         return ApiResponse.of(MEETING_CREATED.getMessage());
     }
 
-    @Operation(summary = "현재 모임 목록 조회", description = "crewId를 받아, 현재 모임 목록을 반환한다.")
+    @Operation(summary = "현재 일정 목록 조회", description = "crewId를 받아, 현재 일정 목록을 반환한다.")
     @GetMapping("/{crewId}/meetings/present")
     public ApiResponse getPresentMeetings(@PathVariable Long crewId, @AuthenticationPrincipal UserPrincipal userPrincipal) {
         List<MeetingsResponse> meetingsResponses = meetingFacade.getPresentMeetings(crewId, userPrincipal.id());
         return ApiResponse.of(MEETINGS_READ.getMessage(), meetingsResponses);
     }
 
-    @Operation(summary = "과거 모임 목록 조회", description = "crewId를 받아, 과거 모임 목록을 반환한다.")
+    @Operation(summary = "과거 일정 목록 조회", description = "crewId를 받아, 과거 일정 목록을 반환한다.")
     @GetMapping("/{crewId}/meetings/past")
     public ApiResponse getPastMeetings(@PathVariable Long crewId, @AuthenticationPrincipal UserPrincipal userPrincipal) {
         List<MeetingsResponse> meetingsResponses = meetingFacade.getPastMeetings(crewId, userPrincipal.id());
         return ApiResponse.of(MEETINGS_READ.getMessage(), meetingsResponses);
     }
 
-    @Operation(summary = "모임 수정", description = "MeetingUpdateRequest를 받아, 모임을 수정한다.")
+    @Operation(summary = "일정 수정", description = "MeetingUpdateRequest를 받아, 일정을 수정한다.")
     @PatchMapping("/meetings")
     public ApiResponse updateMeeting(@RequestBody MeetingUpdateRequest request, @AuthenticationPrincipal UserPrincipal userPrincipal) {
         meetingFacade.updateMeeting(request, userPrincipal.id());
         return ApiResponse.of(MEETING_UPDATED.getMessage());
     }
 
-    @Operation(summary = "모임 삭제", description = "meetingId를 받아, 모임을 삭제한다.")
+    @Operation(summary = "일정 삭제", description = "meetingId를 받아, 일정을 삭제한다.")
     @DeleteMapping("/meetings/{meetingId}")
     public ApiResponse deleteMeeting(@PathVariable Long meetingId, @AuthenticationPrincipal UserPrincipal userPrincipal) {
         meetingFacade.deleteMeeting(meetingId, userPrincipal.id());
         return ApiResponse.of(MEETING_DELETED.getMessage());
     }
 
-    @Operation(summary = "모임멤버 추가", description = "meetingId를 받아, 모임멤버를 추가한다.")
+    @Operation(summary = "일정멤버 추가", description = "meetingId를 받아, 일정멤버를 추가한다.")
     @PostMapping("/meetings/{meetingId}/members")
     public ApiResponse addMeetingMember(@PathVariable Long meetingId, @AuthenticationPrincipal UserPrincipal userPrincipal) {
         meetingFacade.addMeetingMember(meetingId, userPrincipal.id());
         return ApiResponse.of(MEETING_MEMBER_ADDED.getMessage());
     }
 
-    @Operation(summary = "모임멤버 제거", description = "meetingId를 받아, 모임멤버를 제거한다.")
+    @Operation(summary = "일정멤버 제거", description = "meetingId를 받아, 일정멤버를 제거한다.")
     @DeleteMapping("/meetings/{meetingId}/members")
     public ApiResponse removeMeetingMember(@PathVariable Long meetingId, @AuthenticationPrincipal UserPrincipal userPrincipal) {
         meetingFacade.removeMeetingMember(meetingId, userPrincipal.id());
         return ApiResponse.of(MEETING_MEMBER_REMOVED.getMessage());
     }
 
-    @Operation(summary = "모임멤버 목록 조회", description = "meetingId를 받아, 모임멤버 목록을 조회한다.")
+    @Operation(summary = "일정멤버 목록 조회", description = "meetingId를 받아, 일정멤버 목록을 조회한다.")
     @GetMapping("/meetings/{meetingId}/members")
     public ApiResponse getMeetingMembers(@PathVariable Long meetingId, @AuthenticationPrincipal UserPrincipal userPrincipal) {
         List<MeetingMembersResponse> response = meetingFacade.getMeetingMembers(meetingId, userPrincipal.id());
--- orury-client/src/main/java/org/orury/client/meeting/interfaces/message/MeetingMessage.java
+++ orury-client/src/main/java/org/orury/client/meeting/interfaces/message/MeetingMessage.java
@@ -4,13 +4,13 @@ import lombok.Getter;
 
 @Getter
 public enum MeetingMessage {
-    MEETING_CREATED("모임이 정상적으로 생성되었습니다."),
-    MEETINGS_READ("모임 목록을 정상적으로 조회했습니다."),
-    MEETING_UPDATED("모임이 정상적으로 수정됐습니다."),
-    MEETING_DELETED("모임이 정상적으로 삭제됐습니다."),
-    MEETING_MEMBER_ADDED("모임멤버에 정상적으로 추가됐습니다."),
-    MEETING_MEMBER_REMOVED("모임멤버에서 정상적으로 제거됐습니다."),
-    MEETING_MEMBERS_READ("모임멤버 목록을 정상적으로 조회했습니다.");
+    MEETING_CREATED("일정이 정상적으로 생성되었습니다."),
+    MEETINGS_READ("일정 목록을 정상적으로 조회했습니다."),
+    MEETING_UPDATED("일정이 정상적으로 수정됐습니다."),
+    MEETING_DELETED("일정이 정상적으로 삭제됐습니다."),
+    MEETING_MEMBER_ADDED("일정멤버에 정상적으로 추가됐습니다."),
+    MEETING_MEMBER_REMOVED("일정멤버에서 정상적으로 제거됐습니다."),
+    MEETING_MEMBERS_READ("일정멤버 목록을 정상적으로 조회했습니다.");
 
     private final String message;
 
