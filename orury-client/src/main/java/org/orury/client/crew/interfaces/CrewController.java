3f3a46ccb0ce9186ded85451a4e1390aa72ad9e0
Yejin Song <yjyjsssong@gmail.com>
2024-03-06 16:23:33 +0900

    Refactor : WithPageResponse말고 Page가 return되도록 함.

--- orury-client/src/main/java/org/orury/client/crew/interfaces/CrewController.java
+++ orury-client/src/main/java/org/orury/client/crew/interfaces/CrewController.java
@@ -6,9 +6,9 @@ import org.orury.client.crew.interfaces.response.CrewResponse;
 import org.orury.client.crew.interfaces.response.CrewsResponseByMyCrew;
 import org.orury.client.crew.interfaces.response.CrewsResponseByRank;
 import org.orury.client.crew.interfaces.response.CrewsResponseByRecommend;
-import org.orury.client.global.WithPageResponse;
 import org.orury.domain.base.converter.ApiResponse;
 import org.orury.domain.user.domain.dto.UserPrincipal;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,24 +29,24 @@ public class CrewController {
 
     @Operation(summary = "크루 랭킹순 조회", description = "크루를 랭킹 순으로 조회한다.")
     @GetMapping("/rank")
-    public ApiResponse getCrewsByRank(@AuthenticationPrincipal UserPrincipal userPrincipal, @RequestParam int page) {
-        WithPageResponse<CrewsResponseByRank> pageResponse = crewFacade.getCrewsByRank(page);
+    public ApiResponse getCrewsByRank(@RequestParam int page, @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        Page<CrewsResponseByRank> pageResponse = crewFacade.getCrewsByRank(page);
 
         return ApiResponse.of(CrewMessage.CREWS_READ.getMessage(), pageResponse);
     }
 
     @Operation(summary = "크루 추천순 조회", description = "크루를 추천 순으로 조회한다.")
     @GetMapping("/recommend")
-    public ApiResponse getCrewsByRecommendation(@AuthenticationPrincipal UserPrincipal userPrincipal, @RequestParam int page) {
-        WithPageResponse<CrewsResponseByRecommend> pageResponse = crewFacade.getCrewsByRecommend(page);
+    public ApiResponse getCrewsByRecommendation(@RequestParam int page, @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        Page<CrewsResponseByRecommend> pageResponse = crewFacade.getCrewsByRecommend(page);
 
         return ApiResponse.of(CrewMessage.CREWS_READ.getMessage(), pageResponse);
     }
 
     @Operation(summary = "내 크루 조회", description = "내가 가입한 크루를 조회한다.")
     @GetMapping("/mycrew")
-    public ApiResponse getMyCrews(@AuthenticationPrincipal UserPrincipal userPrincipal, @RequestParam int page) {
-        WithPageResponse<CrewsResponseByMyCrew> pageResponse = crewFacade.getMyCrews(userPrincipal.id(), page);
+    public ApiResponse getMyCrews(@RequestParam int page, @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        Page<CrewsResponseByMyCrew> pageResponse = crewFacade.getMyCrews(userPrincipal.id(), page);
 
         return ApiResponse.of(CrewMessage.CREWS_READ.getMessage(), pageResponse);
     }
@@ -54,7 +54,7 @@ public class CrewController {
     @Operation(summary = "크루 상세 조회", description = "크루를 상세 조회한다. 크루 가입 여부에 따라 가입o-> 크루 일정 조회/ 가입x-> 크루 소개글, 크루 가입 버튼이 뜬다." +
             "가입 여부에 따른 API 호출은 어디에서 할지 프론트와 상의해야함.")
     @GetMapping("/{crewId}")
-    public ApiResponse getCrewByCrewId(@AuthenticationPrincipal UserPrincipal userPrincipal, @PathVariable Long crewId) {
+    public ApiResponse getCrewByCrewId(@PathVariable Long crewId, @AuthenticationPrincipal UserPrincipal userPrincipal) {
         CrewResponse response = crewFacade.getCrewByCrewId(userPrincipal.id(), crewId);
 
         return ApiResponse.of(CrewMessage.CREW_READ.getMessage(), response);
