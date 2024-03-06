70a5dbe98f0311190c183a7d4363634500ca8acb
Yejin Song <yjyjsssong@gmail.com>
2024-03-06 16:19:07 +0900

    feat: CrewMemberPK의 UserId로 CrewMember 조회

--- orury-domain/src/main/java/org/orury/domain/crew/infrastructures/CrewMemberRepository.java
+++ orury-domain/src/main/java/org/orury/domain/crew/infrastructures/CrewMemberRepository.java
@@ -4,5 +4,9 @@ import org.orury.domain.crew.domain.entity.CrewMember;
 import org.orury.domain.crew.domain.entity.CrewMemberPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CrewMemberRepository extends JpaRepository<CrewMember, CrewMemberPK> {
+
+    List<CrewMember> findByCrewMemberPK_UserId(Long userId);
 }
