d8b448bbfee7d17568b44048b4f987ddaa54ebbe
Chan <ieejo716@naver.com>
2024-03-06 17:02:31 +0900

    Feature : 유저 리프레시 토큰 제거 로직 구현

--- orury-admin/src/main/java/org/orury/admin/admin/application/AdminFacade.java
+++ orury-admin/src/main/java/org/orury/admin/admin/application/AdminFacade.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.orury.admin.post.application.PostService;
 import org.orury.admin.user.UserService;
 import org.orury.domain.admin.domain.dto.AdminDto;
+import org.orury.domain.auth.domain.RefreshTokenStore;
 import org.orury.domain.post.domain.dto.PostDto;
 import org.orury.domain.user.domain.dto.UserDto;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,7 @@ public class AdminFacade {
     private final AdminService adminService;
     private final PostService postService;
     private final UserService userService;
+    private final RefreshTokenStore refreshTokenStore;
 
     public AdminDto getAdmin(Long adminId) {
         return adminService.getAdmin(adminId);
@@ -38,6 +40,7 @@ public class AdminFacade {
 
     public void banUser(Long userId) {
         var user = userService.getUser(userId);
+        refreshTokenStore.delete(userId);
         userService.banUser(user);
     }
 
