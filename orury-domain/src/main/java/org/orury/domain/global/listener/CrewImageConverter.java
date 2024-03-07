6fa2b279acfd83570f70d95cf90664dd0966b515
Chan <ieejo716@naver.com>
2024-03-07 15:35:07 +0900

    Feature : 크루 생성하면 사용하세요

---
+++ orury-domain/src/main/java/org/orury/domain/global/listener/CrewImageConverter.java
@@ -0,0 +1,9 @@
+package org.orury.domain.global.listener;
+
+// TODO 크루 적용되면 주석풀고 사용하시면 됩니다.
+//public class CrewImageConverter extends EntityImageConverter {
+//    @Override
+//    protected S3Folder domainName() {
+//        return S3Folder.CREW;
+//    }
+//}
