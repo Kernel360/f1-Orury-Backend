a0bd6f683fc25b42fc1b774a4fa7a58bd33edc81
Chan <ieejo716@naver.com>
2024-03-07 15:34:02 +0900

    Refactor : 이미지 로직 수정
    프로필만 사용하던 메서드를 크루에도 사용할 수 있도록 수정

--- orury-domain/src/main/java/org/orury/domain/global/image/ImageReader.java
+++ orury-domain/src/main/java/org/orury/domain/global/image/ImageReader.java
@@ -5,7 +5,7 @@ import org.orury.common.util.S3Folder;
 import java.util.List;
 
 public interface ImageReader {
-    String getUserImageLink(String profile);
+    String getImageLink(S3Folder domain, String profile);
 
     List<String> getImageLinks(S3Folder domain, List<String> images);
 }
