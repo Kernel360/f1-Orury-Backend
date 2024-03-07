2e7a180ecf203bebe016b01736e40d425bb008f0
Yejin Song <yjyjsssong@gmail.com>
2024-03-07 11:08:40 +0900

    Refactor: 이미지 변환 메서드 삭제

--- orury-domain/src/main/java/org/orury/domain/global/image/ImageReader.java
+++ orury-domain/src/main/java/org/orury/domain/global/image/ImageReader.java
@@ -7,7 +7,5 @@ import java.util.List;
 public interface ImageReader {
     String getUserImageLink(String profile);
 
-    String getImageLink(S3Folder domain, String image);
-
     List<String> getImageLinks(S3Folder domain, List<String> images);
 }
