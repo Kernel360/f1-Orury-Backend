b21a552361b1aab0561d535293df2e31431ac22e
GBGreenBravo <gbgreenbravo@gmail.com>
2024-03-11 17:38:17 +0900

    db : 크루 Table 변경 & 크루 태그Table 생성

---
+++ orury-domain/src/main/resources/db/migration/V1.0.26__modify_crew_table_&_add_crew_tag_table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS `crew_tag` (
+                                              `id`	        BIGINT   	       NOT NULL    AUTO_INCREMENT,
+                                              `crew_id`	    BIGINT             NOT NULL,
+                                              `tag`         VARCHAR(20)        NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `crew_tag` ADD CONSTRAINT `FK_crew_TO_crew_tag_1` FOREIGN KEY (`crew_id`)
+    REFERENCES `crew` (`id`)
+    ON DELETE CASCADE;
+
+ALTER TABLE `crew` ADD COLUMN min_age TINYINT NOT NULL;
+ALTER TABLE `crew` ADD COLUMN max_age TINYINT NOT NULL;
+ALTER TABLE `crew` ADD COLUMN gender varchar(1) NOT NULL default 'A';
+ALTER TABLE `crew` ADD COLUMN permission_required TINYINT(1) NOT NULL;
+ALTER TABLE `crew` ADD COLUMN question TEXT NOT NULL;
+ALTER TABLE `crew` ADD COLUMN answer_required TINYINT(1) NOT NULL;
+ALTER TABLE `crew` MODIFY COLUMN `region` VARCHAR(20) NOT NULL;
+
