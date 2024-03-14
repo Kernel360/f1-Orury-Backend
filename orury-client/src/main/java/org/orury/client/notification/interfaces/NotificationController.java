6ca2db222dc3adc314b8ad6245d9e096849802e2
Yejin Song <yjyjsssong@gmail.com>
2024-03-14 19:10:53 +0900

    feat : modify NotificationController

--- orury-client/src/main/java/org/orury/client/notification/interfaces/NotificationController.java
+++ orury-client/src/main/java/org/orury/client/notification/interfaces/NotificationController.java
@@ -1,15 +1,20 @@
 package org.orury.client.notification.interfaces;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.orury.client.notification.application.NotificationService;
+import org.orury.domain.notification.infrastructure.EmitterRepositoryImpl;
 import org.orury.domain.user.domain.dto.UserPrincipal;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.Map;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,14 +26,33 @@ import lombok.extern.slf4j.Slf4j;
 public class NotificationController {
     private final NotificationService notificationService;
 
+    // 서버 메모리에 저장된 emitter, event cache를 파악하기 위해 임시로 추가했습니다.
+    private final EmitterRepositoryImpl emitterRepository;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
 
     @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter subscribe(@AuthenticationPrincipal UserPrincipal userPrincipal) {
-        return notificationService.subscribe(userPrincipal.id());
+    public SseEmitter subscribe(@AuthenticationPrincipal UserPrincipal userPrincipal,
+                                @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "")
+                                String lastEventId) {
+        return notificationService.subscribe(userPrincipal.id(), lastEventId);
     }
 
-    @PostMapping("/send-data/{id}")
-    public void sendData(@PathVariable Long id) {
-        notificationService.notify(id, "test event");
+    // 서버 메모리에 있는 emitter를 확인하기 위해 임시로 추가한 api 입니다.
+    @GetMapping(value = "/getemitter")
+    public String getAllEmitter(@AuthenticationPrincipal UserPrincipal userPrincipal) throws JsonProcessingException {
+
+        Map<String, SseEmitter> map = emitterRepository.getAllEmitters();
+
+        return objectMapper.writeValueAsString(map);
+    }
+
+    // 서버 메모리에 있는 event를 확인하기 위해 임시로 추가한 api 입니다.
+    @GetMapping(value = "/getevent")
+    public String getAll(@AuthenticationPrincipal UserPrincipal userPrincipal) throws JsonProcessingException {
+
+        Map<String, Object> map = emitterRepository.getAllEvents();
+
+        return objectMapper.writeValueAsString(map);
     }
 }
