aade0aed42b8ebdae8d68deff1204f5b0c03eb08
Yejin Song <yjyjsssong@gmail.com>
2024-03-14 16:03:40 +0900

    feat : create NotificationRepository 선언/구현부

--- orury-domain/src/main/java/org/orury/domain/notification/infrastructure/NotificationRepository.java
+++ orury-domain/src/main/java/org/orury/domain/notification/infrastructure/NotificationRepository.java
@@ -1,5 +1,6 @@
 package org.orury.domain.notification.infrastructure;
 
+import org.orury.domain.notification.domain.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
