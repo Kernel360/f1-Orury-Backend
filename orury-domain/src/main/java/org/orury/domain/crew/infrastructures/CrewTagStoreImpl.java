955a65f72bbb524a384aa35c973d1662488ac36e
GBGreenBravo <gbgreenbravo@gmail.com>
2024-03-14 15:49:08 +0900

    feat : (Infrastructures) CrewTag Store/Repository

--- orury-domain/src/main/java/org/orury/domain/crew/infrastructures/CrewTagStoreImpl.java
+++ orury-domain/src/main/java/org/orury/domain/crew/infrastructures/CrewTagStoreImpl.java
@@ -7,6 +7,7 @@ import org.orury.domain.crew.domain.entity.CrewTag;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 @RequiredArgsConstructor
@@ -16,6 +17,16 @@ public class CrewTagStoreImpl implements CrewTagStore {
     @Override
     public void addTags(Crew crew, List<String> tags) {
         tags.stream()
+                .map(tag -> CrewTag.of(null, crew, tag))
+                .forEach(crewTagRepository::save);
+    }
+
+    @Override
+    public void updateTags(List<String> oldTags, List<String> newTags, Crew crew) {
+        if (Objects.equals(oldTags, newTags))
+            return;
+        crewTagRepository.deleteAllByCrewId(crew.getId());
+        newTags.stream()
                 .map(tag -> CrewTag.of(null, crew, tag))
                 .forEach(crewTagRepository::save);
     }
