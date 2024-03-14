df169035d030ab164df30f92f7d3dc22291c2095
GBGreenBravo <gbgreenbravo@gmail.com>
2024-03-14 15:49:42 +0900

    feat : (Infrastructures) CrewApplication Reader/Store/Repository

--- orury-domain/src/main/java/org/orury/domain/crew/domain/CrewApplicationReader.java
+++ orury-domain/src/main/java/org/orury/domain/crew/domain/CrewApplicationReader.java
@@ -1,4 +1,13 @@
 package org.orury.domain.crew.domain;
 
+import org.orury.domain.crew.domain.entity.CrewApplication;
+
+import java.util.List;
+
 public interface CrewApplicationReader {
+    boolean existsByCrewIdAndUserId(Long crewId, Long userId);
+
+    List<CrewApplication> findAllByCrewId(Long crewId);
+
+    int countByUserId(Long userId);
 }
