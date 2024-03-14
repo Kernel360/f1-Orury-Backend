befb4e4c724bcce91f3426f78a5717a104f53906
GBGreenBravo <gbgreenbravo@gmail.com>
2024-03-14 15:49:42 +0900

    feat : (Infrastructures) CrewApplication Reader/Store/Repository

--- orury-domain/src/main/java/org/orury/domain/crew/domain/CrewApplicationStore.java
+++ orury-domain/src/main/java/org/orury/domain/crew/domain/CrewApplicationStore.java
@@ -1,4 +1,17 @@
 package org.orury.domain.crew.domain;
 
+import org.orury.domain.crew.domain.dto.CrewDto;
+import org.orury.domain.crew.domain.entity.CrewApplicationPK;
+import org.orury.domain.user.domain.dto.UserDto;
+
 public interface CrewApplicationStore {
+    void save(CrewDto crewDto, UserDto userDto, String answer);
+
+    void approve(Long crewId, Long userId);
+
+    void approve(CrewApplicationPK crewApplicationPK);
+
+    void delete(Long crewId, Long userId);
+
+    void delete(CrewApplicationPK crewApplicationPK);
 }
