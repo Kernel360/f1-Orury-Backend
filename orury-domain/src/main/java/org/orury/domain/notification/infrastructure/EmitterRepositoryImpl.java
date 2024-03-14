7e5eab86409116c1ec607be085a54db155d25cd6
Yejin Song <yjyjsssong@gmail.com>
2024-03-14 16:03:18 +0900

    feat : create EmitterRepository 선언/구현부

--- orury-domain/src/main/java/org/orury/domain/notification/infrastructure/EmitterRepositoryImpl.java
+++ orury-domain/src/main/java/org/orury/domain/notification/infrastructure/EmitterRepositoryImpl.java
@@ -5,42 +5,77 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
-public class EmitterRepositoryImpl {
+public class EmitterRepositoryImpl implements EmitterRepository {
 
-    // 모든 Emitters를 저장하는 ConcurrentHashMap
-    private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
+    private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
+    private final Map<String, Object> eventCache = new ConcurrentHashMap<>();
 
-    /**
-     * 주어진 아이디와 이미터를 저장
-     *
-     * @param id      - 사용자 아이디.
-     * @param emitter - 이벤트 Emitter.
-     */
-    public void save(Long id, SseEmitter emitter) {
-        emitters.put(id, emitter);
+    @Override
+    public SseEmitter save(String emitterId, SseEmitter sseEmitter) {
+        emitters.put(emitterId, sseEmitter);
+        return sseEmitter;
     }
 
-    /**
-     * 주어진 아이디의 Emitter를 제거
-     *
-     * @param id - 사용자 아이디.
-     */
-    public void deleteById(Long id) {
-        emitters.remove(id);
+    @Override
+    public void saveEventCache(String eventCacheId, Object event) {
+        eventCache.put(eventCacheId, event);
     }
 
-    /**
-     * 주어진 아이디의 Emitter를 가져옴.
-     *
-     * @param id - 사용자 아이디.
-     * @return SseEmitter - 이벤트 Emitter.
-     */
-    public SseEmitter get(Long id) {
-        return emitters.get(id);
+    @Override
+    public Map<String, SseEmitter> findAllEmitterStartWithByUserId(Long userId) {
+        // 해당 회원과 관련된 모든 Emitter 조회
+        return emitters.entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith(userId.toString()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
+
+    @Override
+    public Map<String, Object> findAllEventCacheStartWithByUserId(Long userId) {
+        // 해당 회원과 관련된 모든 EventCache 조회
+        return eventCache.entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith(userId.toString()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+    }
+
+    // TODO : 맵에 저장된 모든 Emitter 조회하는 메소드 구현
+    // TODO : 맵에 저장된 모든 EventCache 조회하는 메소드 구현
+
+    @Override
+    public void deleteEmitterById(String emitterId) {
+        emitters.remove(emitterId);
+    }
+
+    @Override
+    public void deleteAllEmitterStartWithUserId(String userId) {
+        // 해당 회원과 관련된 모든 Emitter 삭제
+        emitters.forEach(
+                (key, emitter) -> {
+                    if (key.startsWith(userId.toString())) {
+                        emitters.remove(key);
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void deleteAllEventCacheStartWithUserId(String userId) {
+        // 해당 회원과 관련된 모든 EventCache 삭제
+        // 회원 탈퇴 등에 사용
+        eventCache.forEach(
+                (key, emitter) -> {
+                    if (key.startsWith(userId.toString())) {
+                        eventCache.remove(key);
+                    }
+                }
+        );
+    }
+
+    // TODO:  Notification의 isRead가 1이 되었을 때 해당 Notification을 가지고 있는 모든 이벤트 캐시도 삭제될 수 있는 메소드 추가하기
 }
