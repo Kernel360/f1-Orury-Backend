1786331de6f0e99109cb72c0d42ed35c629fd125
GBGreenBravo <gbgreenbravo@gmail.com>
2024-03-15 14:07:36 +0900

    chore : 태그 글자수 5->6으로 변경

--- orury-client/src/main/java/org/orury/client/crew/interfaces/request/CrewRequest.java
+++ orury-client/src/main/java/org/orury/client/crew/interfaces/request/CrewRequest.java
@@ -44,7 +44,7 @@ public record CrewRequest(
         boolean answerRequired,
 
         @Size(min = 1, max = 3, message = "태그는 최소 1개, 최대 3개까지만 추가할 수 있습니다.")
-        List<@Size(min = 1, max = 5, message = "태그 길이는 1~5 글자여야 합니다.") String> tags
+        List<@Size(min = 1, max = 6, message = "태그 길이는 공백 포함 6글자 이내여야 합니다.") String> tags
 ) {
     public static CrewRequest of(
             String name,
