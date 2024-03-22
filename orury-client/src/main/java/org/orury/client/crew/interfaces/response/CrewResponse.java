71581d5e88fdcace8540750b7b4cfc00ea5ddc88
Hyungjoon Cho <selcon1996@gmail.com>
2024-03-22 12:41:07 +0900

    Feat : 크루장 닉네임 추가

--- orury-client/src/main/java/org/orury/client/crew/interfaces/response/CrewResponse.java
+++ orury-client/src/main/java/org/orury/client/crew/interfaces/response/CrewResponse.java
@@ -11,6 +11,7 @@ import java.util.List;
 public record CrewResponse(
         Long id,
         String name,
+        String headName,
         int memberCount,
         int capacity,
         Region region,
@@ -30,6 +31,7 @@ public record CrewResponse(
         return new CrewResponse(
                 crewDto.id(),
                 crewDto.name(),
+                crewDto.userDto().nickname(),
                 crewDto.memberCount(),
                 crewDto.capacity(),
                 crewDto.region(),
